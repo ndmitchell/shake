db160f311ee9c67af6111c3a98e117b35e16db87
Neil Mitchell <unknown>
2011-12-20 15:21:02 +0000

    Fix a missing \ escape in haddock docs

--- Development/Shake.hs
+++ Development/Shake.hs
@@ -9,7 +9,7 @@
 --
 --main = 'shake' 'shakeOptions' $ do
 --    'want' [\"result.tar\"]
---    \"*.tar\" *> \out -> do
+--    \"*.tar\" *> \\out -> do
 --        contents <- 'readFileLines' $ replaceExtension out \"txt\"
 --        'need' contents
 --        'system'' \"tar\" $ [\"-cf\",out] ++ contents
