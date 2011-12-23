2824a21d0477fb99e8442afed8f039b8d82e222f
Neil Mitchell <unknown>
2011-12-23 11:53:45 +0000

    Add some assertions about file pattern matches

--- Examples/Test/Basic1.hs
+++ Examples/Test/Basic1.hs
@@ -14,6 +14,14 @@ main = shaken test $ \obj -> do
 
 
 test build obj = do
+    let f b pat file = assert (b == (pat ?== file)) $ show pat ++ " ?== " ++ show file ++ "\nEXPECTED: " ++ show b
+    f True "//*.c" "foo/bar/baz.c"
+    f True "*.c" "baz.c"
+    f True "//*.c" "baz.c"
+    f True "test.c" "test.c"
+    f False "*.c" "foor/bar.c"
+    f False "*/*.c" "foo/bar/baz.c"
+
     writeFile (obj "A.txt") "AAA"
     writeFile (obj "B.txt") "BBB"
     build []
