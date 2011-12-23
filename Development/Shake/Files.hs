8ee4549e476afc47efda0db4e11f04bfe42780da
Neil Mitchell <unknown>
2011-12-23 12:26:55 +0000

    Add documentation and improve the error messages for *>>

--- Development/Shake/Files.hs
+++ Development/Shake/Files.hs
@@ -34,11 +34,22 @@ instance Rule Files FileTimes where
     validStored (Files xs) (FileTimes ts) = fmap (== map Just ts) $ mapM getModTimeMaybe xs
 
 
--- | All the file patterns must be compatible, so same // and * placements.
---   Can then substitute one to get the other.
+-- | Define a rule for building multiple files at the same time.
+--   As an example, a single invokation of GHC produces both @.hi@ and @.o@ files:
+--
+-- > ["*.o","*.hi"] *>> \[o,hi] -> do
+-- >    let hs = replaceExtension o "hs"
+-- >    need ... -- all files the .hs import
+-- >    system' "ghc" ["-c",hs]
+--
+--   However, in practice, it's usually easier to define rules with '*>' and make the @.hi@ depend
+--   on the @.o@. When defining rules that build multiple files, all the 'FilePattern' values must
+--   have the same sequence of @\/\/@ and @*@ wildcards in the same order.
 (*>>) :: [FilePattern] -> ([FilePath] -> Action ()) -> Rules ()
 ps *>> act
-    | not $ compatible ps = error "all must be compatible"
+    | not $ compatible ps = error $
+        "All patterns to *>> must have the same number and position of // and * wildcards\n" ++
+        unwords ps
     | otherwise = do
         forM ps $ \p ->
             p *> \file -> do
