f42c60c261a4d0f5b15b61e1f4310380e67665d2
Neil Mitchell <unknown>
2012-01-02 13:35:52 +0000

    Change the Oracle to being monadic, and make it use askOracle

--- Development/Shake/Oracle.hs
+++ Development/Shake/Oracle.hs
@@ -1,24 +1,24 @@
 {-# LANGUAGE MultiParamTypeClasses, GeneralizedNewtypeDeriving, DeriveDataTypeable, ScopedTypeVariables #-}
 
 module Development.Shake.Oracle(
-    addOracle, getOracle
+    addOracle, askOracle
     ) where
 
 import Control.DeepSeq
-import Control.Monad
 import Data.Binary
 import Data.Hashable
-import Data.Maybe
 import Data.Typeable
 
 import Development.Shake.Core
 
 
 newtype Question = Question [String]
-    deriving (Show,Typeable,Eq,Hashable,Binary,NFData)
+    deriving (Typeable,Eq,Hashable,Binary,NFData)
 newtype Answer = Answer [String]
     deriving (Show,Typeable,Eq,Hashable,Binary,NFData)
 
+instance Show Question where
+    show (Question xs) = "Oracle " ++ unwords xs
 
 instance Rule Question Answer where
     validStored _ _ = return False
@@ -26,16 +26,16 @@ instance Rule Question Answer where
 
 -- | Add extra information which your build should depend on. For example:
 --
--- > addOracle ["ghc"] ["7.2.1"]
--- > addOracle ["ghc-pkg","shake"] ["1.0"]
+-- > addOracle ["ghc"] $ return ["7.2.1"]
+-- > addOracle ["ghc-pkg","shake"] $ return ["1.0"]
 --
 --   If a rule depends on the GHC version, it can then use @'getOracle' ["ghc"]@, and
 --   if the GHC version changes, the rule will rebuild.
-addOracle :: [String] -> [String] -> Rules ()
-addOracle question answer = rule $ \(Question q) ->
-    if q == question then Just $ return $ Answer answer else Nothing
+addOracle :: [String] -> Action [String] -> Rules ()
+addOracle question act = rule $ \(Question q) ->
+    if q == question then Just $ fmap Answer act else Nothing
 
 
 -- | Get information previously added with 'addOracle'.
-getOracle :: [String] -> Action [String]
-getOracle question = do Answer answer <- apply1 $ Question question; return answer
+askOracle :: [String] -> Action [String]
+askOracle question = do Answer answer <- apply1 $ Question question; return answer
