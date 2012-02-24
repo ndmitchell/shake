ef1f403374b32e7ad50752f5bcbc6085d2afb466
Neil Mitchell <unknown>
2012-02-24 10:27:15 +0000

    Reindent and reformat the report module (purely personal preference)

--- Development/Shake/Report.hs
+++ Development/Shake/Report.hs
@@ -1,8 +1,7 @@
-module Development.Shake.Report
-       ( buildReport -- :: String -> FilePath -> IO ()
-       ) where
-import Text.StringTemplate as T
 
+module Development.Shake.Report(buildReport) where
+
+import Text.StringTemplate as T
 import System.FilePath
 import Paths_shake
 
@@ -11,21 +10,21 @@ import Paths_shake
 -- profiling data in JSON format.
 buildReport :: String -> FilePath -> IO ()
 buildReport json out = do
-  htmlDir <- getDataFileName "html"
-  report  <- readFile $ htmlDir </> "report.html"
-  bcss    <- readFile $ htmlDir </> "bootstrap.min.css"
-  brcss   <- readFile $ htmlDir </> "bootstrap-responsive.min.css"
-  bjs     <- readFile $ htmlDir </> "bootstrap.min.js"
-  jquery  <- readFile $ htmlDir </> "jquery-1.6.4.min.js"
-  flot    <- readFile $ htmlDir </> "jquery.flot.min.js"
-  shakejs <- readFile $ htmlDir </> "shake.js"
+    htmlDir <- getDataFileName "html"
+    report  <- readFile $ htmlDir </> "report.html"
+    bcss    <- readFile $ htmlDir </> "bootstrap.min.css"
+    brcss   <- readFile $ htmlDir </> "bootstrap-responsive.min.css"
+    bjs     <- readFile $ htmlDir </> "bootstrap.min.js"
+    jquery  <- readFile $ htmlDir </> "jquery-1.6.4.min.js"
+    flot    <- readFile $ htmlDir </> "jquery.flot.min.js"
+    shakejs <- readFile $ htmlDir </> "shake.js"
 
-  let t = T.setAttribute "bootstrapcss" bcss
-        $ T.setAttribute "bootstraprcss" brcss
-        $ T.setAttribute "bootstrapjs" bjs
-        $ T.setAttribute "jquery" jquery
-        $ T.setAttribute "flot" flot
-        $ T.setAttribute "shakejs" shakejs
-        $ T.setAttribute "shakedump" json
-        $ T.newSTMP report :: StringTemplate String
-  writeFile out $ T.render t
+    let t = T.setAttribute "bootstrapcss" bcss
+          $ T.setAttribute "bootstraprcss" brcss
+          $ T.setAttribute "bootstrapjs" bjs
+          $ T.setAttribute "jquery" jquery
+          $ T.setAttribute "flot" flot
+          $ T.setAttribute "shakejs" shakejs
+          $ T.setAttribute "shakedump" json
+          $ T.newSTMP report :: StringTemplate String
+    writeFile out $ T.render t
