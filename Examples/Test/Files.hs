1d4876905283d9ea8e46dbe3422be5d23527e723
Neil Mitchell <unknown>
2012-02-25 15:52:51 +0000

    Add test for ?>>

--- Examples/Test/Files.hs
+++ Examples/Test/Files.hs
@@ -4,12 +4,18 @@ module Examples.Test.Files(main) where
 import Development.Shake
 import Development.Shake.FilePattern
 import Examples.Util
+import Control.Monad
 import Data.List
 
 
 main = shaken test $ \args obj -> do
     want $ map obj ["even.txt","odd.txt"]
-    map obj ["even.txt","odd.txt"] *>> \[evens,odds] -> do
+
+    let deps = map obj ["even.txt","odd.txt"]
+    let def | "fun" `elem` args = (?>>) (\x -> if x `elem` deps then Just deps else Nothing)
+            | otherwise = (*>>) deps
+
+    def $ \[evens,odds] -> do
         src <- readFileLines $ obj "numbers.txt"
         let (es,os) = partition even $ map read src
         writeFileLines evens $ map show es
@@ -26,8 +32,10 @@ test build obj = do
     substitute ["","test","da"] "//*a*.txt" === "testada.txt"
     substitute  ["foo/bar/","test"] "//*a.txt" === "foo/bar/testa.txt"
 
-    let nums = unlines . map show
-    writeFile (obj "numbers.txt") $ nums [1,2,4,5,2,3,1]
-    build []
-    assertContents (obj "even.txt") $ nums [2,4,2]
-    assertContents (obj "odd.txt" ) $ nums [1,5,3,1]
+    forM_ [[],["fun"]] $ \args -> do
+        sleepFileTime
+        let nums = unlines . map show
+        writeFile (obj "numbers.txt") $ nums [1,2,4,5,2,3,1]
+        build args
+        assertContents (obj "even.txt") $ nums [2,4,2]
+        assertContents (obj "odd.txt" ) $ nums [1,5,3,1]
