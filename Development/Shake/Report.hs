d30101f436f0b8fb6c117bade91b3abc35d4fa70
Neil Mitchell <unknown>
2012-07-10 11:05:26 +0000

    Rewrite the profile output and template engine to make it easier to live preview the results

--- Development/Shake/Report.hs
+++ Development/Shake/Report.hs
@@ -3,6 +3,8 @@
 module Development.Shake.Report(buildReport) where
 
 import Control.Monad
+import Data.Char
+import Data.List
 import System.FilePath
 import Paths_shake
 
@@ -13,14 +15,22 @@ buildReport :: String -> FilePath -> IO ()
 buildReport json out = do
     htmlDir <- getDataFileName "html"
     report <- readFile $ htmlDir </> "report.html"
-    let f name | name == "data" = return json
+    let f name | name == "data.js" = return $ "var shake = \n" ++ json
                | otherwise = readFile $ htmlDir </> name
     writeFile out =<< runTemplate f report
 
 
--- | Template Engine
+-- | Template Engine. Perform the following replacements on a line basis:
+--
+-- * <script src="foo"></script> ==> <script>[[foo]]</script>
+--
+-- * <link href="foo" rel="stylesheet" type="text/css" /> ==> <style type="text/css">[[foo]]</style>
 runTemplate :: Monad m => (String -> m String) -> String -> m String
-runTemplate ask ('$':'$':xs) = liftM ('$':) $ runTemplate ask xs
-runTemplate ask ('$':xs) | (name,_:rest) <- break (== '$') xs = liftM2 (++) (ask name) (runTemplate ask rest)
-runTemplate ask (x:xs) = liftM (x:) $ runTemplate ask xs
-runTemplate ask [] = return []
+runTemplate ask = liftM unlines . mapM f . lines
+    where
+        f x | Just file <- stripPrefix "<script src=\"" y = do res <- grab file; return $ "<script>\n" ++ res ++ "\n</script>"
+            | Just file <- stripPrefix "<link href=\"" y = do res <- grab file; return $ "<style type=\"text/css\">\n" ++ res ++ "\n</style>"
+            | otherwise = return x
+            where
+                y = dropWhile isSpace x
+                grab = ask . takeWhile (/= '\"')
