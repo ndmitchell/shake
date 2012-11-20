8064e479814c0ee42baa258a2c1cd9dcec54f661
Neil Mitchell <unknown>
2012-11-20 10:46:49 +0000

    Delete the integration tests, they are done in Haskell now

--- tests/integration/basic01/shakefile.hs
+++
@@ -1,9 +0,0 @@
-import Development.Shake
-
-main = shake shakeOptions $ do
-  want ["AB.txt"]
-  "AB.txt" *> \out -> do
-    need ["A.txt", "B.txt"]
-    text1 <- readFile' "A.txt"
-    text2 <- readFile' "B.txt"
-    writeFile' out (text1 ++ text2)
