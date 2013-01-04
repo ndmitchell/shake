d17f1e85c955782a44279ab8d9b89a6d8b8ede41
Neil Mitchell <unknown>
2013-01-04 22:48:12 +0000

    Add a Classes module to reexport the data types

---
+++ Development/Shake/Classes.hs
@@ -0,0 +1,11 @@
+
+-- | This module reexports the six necessary type classes that every 'Rule' type must support.
+--   You can use this module to define new rules without depending on the @binary@, @deepseq@ and @hashable@ packages.
+module Development.Shake.Classes(
+    Show,Typeable,Eq,Hashable,Binary,NFData
+    ) where
+
+import Data.Hashable
+import Data.Typeable
+import Data.Binary
+import Control.DeepSeq
