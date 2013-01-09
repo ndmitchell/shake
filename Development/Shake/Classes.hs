e8e85bdfc4ca341c371ffecc6ed1a0ad5c61a006
Neil Mitchell <unknown>
2013-01-09 16:41:26 +0000

    Export the class bodies from Classes

--- Development/Shake/Classes.hs
+++ Development/Shake/Classes.hs
@@ -3,7 +3,7 @@
 -- | This module reexports the six necessary type classes that every 'Rule' type must support.
 --   You can use this module to define new rules without depending on the @binary@, @deepseq@ and @hashable@ packages.
 module Development.Shake.Classes(
-    Show,Typeable,Eq,Hashable,Binary,NFData
+    Show(..), Typeable(..), Eq(..), Hashable(..), Binary(..), NFData(..)
     ) where
 
 import Data.Hashable
