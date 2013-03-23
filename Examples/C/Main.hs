e414d985ec637bb6154c7183de591573d743ef4c
Neil Mitchell <ndmitchell@gmail.com>
2013-03-23 09:50:11 +0000

    Replace the C example from sys to cmd

--- Examples/C/Main.hs
+++ Examples/C/Main.hs
@@ -3,7 +3,7 @@ module Examples.C.Main(main) where
 
 import Development.Shake
 import Development.Shake.FilePath
-import Development.Shake.Sys
+import Development.Shake.Command
 import Examples.Util
 
 main = shaken noTest $ \args obj -> do
@@ -14,16 +14,16 @@ main = shaken noTest $ \args obj -> do
         cs <- getDirectoryFiles src ["*.c"]
         let os = map (obj . (<.> "o")) cs
         need os
-        sys "gcc -o" [out] os
+        cmd "gcc -o" [out] os
 
     obj "*.c.o" *> \out -> do
         let c = src </> takeBaseName out
         need [c]
         headers <- cIncludes c
         need $ map ((</>) src . takeFileName) headers
-        sys "gcc -o" [out] "-c" [c]
+        cmd "gcc -o" [out] "-c" [c]
 
 cIncludes :: FilePath -> Action [FilePath]
 cIncludes x = do
-    (stdout,_) <- systemOutput "gcc" ["-MM",x]
+    Stdout stdout <- cmd "gcc" ["-MM",x]
     return $ drop 2 $ words stdout
