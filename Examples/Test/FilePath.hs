b47c2a35864057168dd0b0338ce59b2e56f18dae
Neil Mitchell <ndmitchell@gmail.com>
2013-03-29 09:31:01 +0000

    Additional FilePath tests

--- Examples/Test/FilePath.hs
+++ Examples/Test/FilePath.hs
@@ -15,3 +15,16 @@ test build obj = do
     normalise "../../foo" === "../../foo"
     normalise "foo/bar/../../neil" === "neil"
     normalise "foo/../bar/../neil" === "neil"
+
+    dropDirectory1 "aaa/bbb" === "bbb"
+    dropDirectory1 "aaa/" === ""
+    dropDirectory1 "aaa" === ""
+    dropDirectory1 "" === ""
+
+    takeDirectory1 "aaa/bbb" === "aaa"
+    takeDirectory1 "aaa/" === "aaa"
+    takeDirectory1 "aaa" === "aaa"
+
+    combine "aaa/bbb" "ccc" === "aaa/bbb/ccc"
+    combine "aaa/bbb" "./ccc" === "aaa/bbb/ccc"
+    combine "aaa/bbb" "../ccc" === "aaa/ccc"
