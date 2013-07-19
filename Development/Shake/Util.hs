12f945b028024f039e89c6787816656f79df523a
Neil Mitchell <ndmitchell@gmail.com>
2013-07-19 07:50:04 +0100

    Add a new utility module

---
+++ Development/Shake/Util.hs
@@ -0,0 +1,16 @@
+{-# LANGUAGE BangPatterns #-}
+
+module Development.Shake.Util(
+    module Development.Shake.Util
+    ) where
+
+import Data.IORef
+
+-- Two 's because GHC 7.6 has a strict modifyIORef
+modifyIORef'' :: IORef a -> (a -> a) -> IO ()
+modifyIORef'' ref f = do
+    x <- readIORef ref
+    writeIORef'' ref $ f x
+
+writeIORef'' :: IORef a -> a -> IO ()
+writeIORef'' ref !x = writeIORef ref x
