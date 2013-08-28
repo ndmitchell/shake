ddfa209185a2c16c5eae0cd54aa319ae72cf3716
Neil Mitchell <ndmitchell@gmail.com>
2013-08-28 16:21:52 +0100

    Retry the throttle up to three times

--- Examples/Test/Throttle.hs
+++ Examples/Test/Throttle.hs
@@ -17,9 +17,18 @@ main = shaken test $ \args obj -> do
 
 test build obj = do
     forM_ [[],["-j8"]] $ \flags -> do
-        build ["clean"]
-        (s, _) <- duration $ build []
-        assert (s >= 0.6 && s < 0.7) $
+        -- we are sometimes over the window if the machine is "a bit loaded" at some particular time
+        -- therefore we rerun the test three times, and only fail if it fails on all of them
+        flip loop 3 $ \i -> do
+            build ["clean"]
+            (s, _) <- duration $ build []
             -- the 0.1s cap is a guess at an upper bound for how long everything else should take
             -- and should be raised on slower machines
-            "Bad throttling, expected to take 0.6s + computation time (cap of 0.1s), took " ++ show s ++ "s"
+            let good = s >= 0.6 && s < 0.7
+            if good then return $ Right ()
+             else if i > 1 then do
+                putStrLn $ "Throttle failed (took " ++ show s ++ "s), retrying"
+                return $ Left (i-1)
+             else do
+                assert False $ "Bad throttling, expected to take 0.6s + computation time (cap of 0.1s), took " ++ show s ++ "s (three times in a row)"
+                return $ Right ()
