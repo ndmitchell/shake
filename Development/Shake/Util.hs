7e2f2a55e3407c9e842df02e5ee070bea148c102
Neil Mitchell <ndmitchell@gmail.com>
2013-10-25 07:50:22 +0100

    Add a Util module, for Makefile parsing at the moment

---
+++ Development/Shake/Util.hs
@@ -0,0 +1,24 @@
+{-# LANGUAGE BangPatterns, GeneralizedNewtypeDeriving #-}
+
+module Development.Shake.Util(
+    parseMakefile, needMakefileDependencies
+    ) where
+
+import Development.Shake
+import Data.List
+
+
+parseMakefile :: String -> [(FilePath, [FilePath])]
+parseMakefile = concatMap f . join . lines
+    where
+        join (x1:x2:xs) | "\\" `isSuffixOf` x1 = join $ (init x1 ++ x2) : xs
+        join (x:xs) = x : join xs
+        join [] = []
+
+        f x = [(a, words $ drop 1 b) | a <- words a]
+            where (a,b) = break (== ':') $ takeWhile (/= '#') x
+
+
+needMakefileDependencies :: FilePath -> Action ()
+needMakefileDependencies file = need . concatMap snd . parseMakefile =<< liftIO (readFile file)
+
