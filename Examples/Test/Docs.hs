12236f6c02209b68ffd737c094806822e1d1ea3f
Neil Mitchell <ndmitchell@gmail.com>
2013-12-11 11:19:54 +0000

    Allow deprecations in the doc tests, since Sys is now deprecated

--- Examples/Test/Docs.hs
+++ Examples/Test/Docs.hs
@@ -45,6 +45,7 @@ main = shaken noTest $ \args obj -> do
             (imports,rest) = partition ("import " `isPrefixOf`) code
         writeFileLines out $
             ["{-# LANGUAGE ConstraintKinds, DeriveDataTypeable, ExtendedDefaultRules, GeneralizedNewtypeDeriving, NoMonomorphismRestriction #-}"
+            ,"{-# OPTIONS_GHC -fno-warn-warnings-deprecations #-}"
             ,"module " ++ takeBaseName out ++ "() where"
             ,"import Control.Concurrent"
             ,"import Control.Monad"
