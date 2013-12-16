74d82946011cfb9936d43c234df67d12dfcba5f7
Neil Mitchell <ndmitchell@gmail.com>
2013-12-16 11:59:40 +0000

    Diagnostics for travis

--- benchmark.sh
+++ benchmark.sh
@@ -8,5 +8,6 @@ git clone https://github.com/martine/ninja
 (cd ninja && ./bootstrap.py)
 export PATH=$PATH:`pwd`/ninja
 
+ls /usr/bin
 (cd ninja && ninja -t clean && date +%H:%M:%S.%N && ninja -j3 && date +%H:%M:%S.%N && ninja -j3 && date +%H:%M:%S.%N)
 (cd ninja && ninja -t clean && date +%H:%M:%S.%N && shake -j3 && date +%H:%M:%S.%N && shake -j3 && date +%H:%M:%S.%N)
