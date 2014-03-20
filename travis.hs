46fdc23b91af97dfab5b5e6c169c0bc27a8b8b7b
Neil Mitchell <ndmitchell@gmail.com>
2014-03-20 19:14:39 +0000

    Point at the right ninja script

--- travis.hs
+++ travis.hs
@@ -11,12 +11,12 @@ main = do
     setCurrentDirectory "ninja"
 
     -- time Ninja
-    cmd "ninja -t clean"
+    cmd "../nin -t clean"
     (ninjaFull, _) <- duration $ cmd "../nin -j3 -d stats"
     (ninjaZero, _) <- duration $ cmd "../nin -j3 -d stats"
 
     -- time Shake
-    cmd "ninja -t clean"
+    cmd "../nin -t clean"
     (shakeFull, _) <- duration $ cmd "shake -j3 --quiet --timings"
     (shakeZero, _) <- duration $ cmd "shake -j3 --quiet --timings"
 
@@ -26,11 +26,10 @@ main = do
 
     let ms x = show $ ceiling $ x * 1000
     putStrLn $ "Ninja was " ++ ms ninjaFull ++ " then " ++ ms ninjaZero
-    putStrLn $ "Shake was " ++ ms ninjaFull ++ " then " ++ ms ninjaZero
+    putStrLn $ "Shake was " ++ ms shakeFull ++ " then " ++ ms shakeZero
 
     when (ninjaFull < shakeFull) $
         error "ERROR: Ninja build was faster than Shake"
 
     when (ninjaZero + 0.1 < shakeZero) $
         error "ERROR: Ninja zero build was more than 0.1s faster than Shake"
-
