97d5acdee578554a61fc11296b8c73abdd8e3015
Jens Petersen <juhpetersen@gmail.com>
2014-03-31 16:35:24 +0900

    build.sh needs -threaded for -qg and -qb
    without this I get:
    
    Linking _shake/build ...
    build: the flag -qg requires the program to be built with -threaded
    build: the flag -qb requires the program to be built with -threaded
    
    This change is probably needed for build.bat too, but I don't use Win so didn't test.

--- docs/manual/build.sh
+++ docs/manual/build.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 mkdir -p _shake
-ghc --make Build.hs -rtsopts "-with-rtsopts=-I0 -qg -qb" -outputdir=_shake -o _shake/build && _shake/build "$@"
+ghc --make Build.hs -threaded -rtsopts "-with-rtsopts=-I0 -qg -qb" -outputdir=_shake -o _shake/build && _shake/build "$@"
