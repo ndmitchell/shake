cf3d2786d3e204f014b2de89931182f047da73ba
Neil Mitchell <ndmitchell@gmail.com>
2014-04-24 10:14:17 +0100

    Add testing for the code in all markdown files, after #111

--- Examples/Test/Docs.hs
+++ Examples/Test/Docs.hs
@@ -39,16 +39,20 @@ main = shaken noTest $ \args obj -> do
 
     obj "Part_*.hs" *> \out -> do
         need ["Examples/Test/Docs.hs"] -- so much of the generator is in this module
-        src <- readFile' $ "dist/doc/html/shake/" ++ reps '_' '-' (drop 5 $ takeBaseName out) ++ ".html"
+        src <- if "_md" `isSuffixOf` takeBaseName out then
+            fmap (findCodeMarkdown . lines) $ readFile' $ "docs/" ++ drop 5 (reverse (drop 3 $ reverse $ takeBaseName out)) ++ ".md"
+         else
+            fmap findCodeHaddock $ readFile' $ "dist/doc/html/shake/" ++ reps '_' '-' (drop 5 $ takeBaseName out) ++ ".html"
         let f i (Stmt x) | whitelist $ head x = []
-                         | otherwise = restmt i $ map undefDots x
-            f i (Expr x) | x `elem` types = ["type Expr_" ++ show i ++ " = " ++ x]
+                         | otherwise = restmt i $ map undefDots $ trims x
+            f i (Expr x) | takeWhile (not . isSpace) x `elem` types = ["type Expr_" ++ show i ++ " = " ++ x]
+                         | "import " `isPrefixOf` x = [x]
                          | otherwise = ["expr_" ++ show i ++ " = (" ++ undefDots x2 ++ ")" | let x2 = trim $ dropComment x, not $ whitelist x2]
-            code = concat $ zipWith f [1..] (nub $ findCode src)
+            code = concat $ zipWith f [1..] (nub src)
             (imports,rest) = partition ("import " `isPrefixOf`) code
         writeFileLines out $
             ["{-# LANGUAGE ConstraintKinds, DeriveDataTypeable, ExtendedDefaultRules, GeneralizedNewtypeDeriving, NoMonomorphismRestriction #-}"
-            ,"{-# OPTIONS_GHC -fno-warn-warnings-deprecations #-}"
+            ,"{-# OPTIONS_GHC -fno-warn-warnings-deprecations -fno-warn-wrong-do-bind #-}"
             ,"module " ++ takeBaseName out ++ "() where"
             ,"import Control.Concurrent"
             ,"import Control.Monad"
@@ -64,9 +68,8 @@ main = shaken noTest $ \args obj -> do
             ,"import Development.Shake.FilePath"
             ,"import System.Console.GetOpt"
             ,"import System.Exit"
-            ,"import System.IO"
-            ,"import " ++ reps '_' '.' (drop 5 $ takeBaseName out)
-            ] ++
+            ,"import System.IO"] ++
+            ["import " ++ reps '_' '.' (drop 5 $ takeBaseName out) | not $ "_md.hs" `isSuffixOf` out] ++
             imports ++
             ["(==>) :: Bool -> Bool -> Bool"
             ,"(==>) = undefined"
@@ -78,21 +81,30 @@ main = shaken noTest $ \args obj -> do
             ,"old = \"\""
             ,"new = \"\""
             ,"myfile = \"\""
+            ,"inputs = [\"\"]"
+            ,"files = [\"\"]"
+            ,"input = \"\""
+            ,"output = \"\""
             ,"opts = shakeOptions"
             ,"result = undefined :: IO (Maybe (Rules ()))"
+            ,"launchMissiles = undefined :: Bool -> IO ()"
+            ,"myVariable = ()"
             ,"instance Eq (OptDescr a)"
-            ,"inputs = [\"\"]"
-            ,"output = \"\""
             ,"(foo,bar,baz) = undefined"
-            ,"((/./),(/../),(//)) = undefined"
-            ] ++
+            ,"str1 = \"\""
+            ,"str2 = \"\""
+            ,"str = \"\""
+            ,"((/./),(/../),(//)) = undefined"] ++
             rest
 
     obj "Files.lst" *> \out -> do
+        need ["Examples/Test/Docs.hs"] -- so much of the generator is in this module
         need [index,obj "Paths_shake.hs"]
         filesHs <- getDirectoryFiles "dist/doc/html/shake" ["Development-*.html"]
-        writeFileLines out $
-            ["Part_" ++ reps '-' '_' (takeBaseName x) | x <- filesHs, not $ "-Classes.html" `isSuffixOf` x]
+        filesMd <- getDirectoryFiles "docs" ["*.md"]
+        writeFileChanged out $ unlines $
+            ["Part_" ++ reps '-' '_' (takeBaseName x) | x <- filesHs, not $ "-Classes.html" `isSuffixOf` x] ++
+            ["Part_" ++ takeBaseName x ++ "_md" | x <- filesMd]
 
     let needModules = do mods <- readFileLines $ obj "Files.lst"; need [obj m <.> "hs" | m <- mods]; return mods
 
@@ -110,24 +122,41 @@ main = shaken noTest $ \args obj -> do
 
 data Code = Stmt [String] | Expr String deriving (Show,Eq)
 
-findCode :: String -> [Code]
-findCode x | Just x <- stripPrefix "<pre>" x = f (Stmt . shift . lines . strip) "</pre>" x
-           | Just x <- stripPrefix "<code>" x = f (Expr . strip) "</code>" x
+findCodeHaddock :: String -> [Code]
+findCodeHaddock x | Just x <- stripPrefix "<pre>" x = f (Stmt . shift . lines . strip) "</pre>" x
+                  | Just x <- stripPrefix "<code>" x = f (Expr . strip) "</code>" x
     where
-        f ctor end x | Just x <- stripPrefix end x = ctor "" : findCode x
+        f ctor end x | Just x <- stripPrefix end x = ctor "" : findCodeHaddock x
         f ctor end (x:xs) = f (ctor . (x:)) end xs
-findCode (x:xs) = findCode xs
-findCode [] = []
+findCodeHaddock (x:xs) = findCodeHaddock xs
+findCodeHaddock [] = []
+
+findCodeMarkdown :: [String] -> [Code]
+findCodeMarkdown (x:xs) | indented x && not (blank x) =
+    let (a,b) = span (\x -> indented x || blank x) (x:xs)
+    in Stmt (map (drop 4) a) : findCodeMarkdown b
+    where
+        indented x = length (takeWhile isSpace x) >= 4
+        blank x = all isSpace x
+findCodeMarkdown (x:xs) = f x ++ findCodeMarkdown xs
+    where
+        f ('`':xs) = let (a,b) = break (== '`') xs in Expr a : f (drop 1 b)
+        f (x:xs) = f xs
+        f [] = []
+findCodeMarkdown [] = []
 
 trim = reverse . dropWhile isSpace . reverse . dropWhile isSpace
+trims = reverse . dropWhile (all isSpace) . reverse . dropWhile (all isSpace)
 
 restmt i ("":xs) = restmt i xs
 restmt i (x:xs) | " ?== " `isInfixOf` x || " == " `isInfixOf` x =
     zipWith (\j x -> "hack_" ++ show i ++ "_" ++ show j ++ " = " ++ x) [1..] (x:xs)
-restmt i (x:xs) | " = " `isInfixOf` x || " | " `isInfixOf` x ||
+restmt i (x:xs) |
+    not ("let" `isPrefixOf` x) && not ("[" `isPrefixOf` x) && (" = " `isInfixOf` x || " | " `isInfixOf` x) ||
     "import " `isPrefixOf` x || "infix" `isPrefixOf` x || "instance " `isPrefixOf` x = map f $ x:xs
     where f x = if takeWhile (not . isSpace) x `elem` dupes then "_" ++ show i ++ "_" ++ x else x
-restmt i xs = ("stmt_" ++ show i ++ " = do") : map ("  " ++) xs
+restmt i xs = ("stmt_" ++ show i ++ " = do") : map ("  " ++) xs ++
+              ["  undefined" | length xs == 1 && ("let" `isPrefixOf` (head xs) || "<-" `isInfixOf` (head xs))]
 
 
 shift :: [String] -> [String]
@@ -169,16 +198,19 @@ italics :: [String]
 italics = words "extension command-name file-name"
 
 whitelist :: String -> Bool
-whitelist x | all (not . isSpace) x && takeExtension x `elem` words ".txt .hi .o .exe .tar .cpp .cfg" = True
+whitelist x | all (not . isSpace) x && takeExtension x `elem` words ".txt .hi .hs .o .exe .tar .cpp .cfg .dep .deps .h .c .html" = True
 whitelist x | elem x $ words $
-    "newtype do MyFile.txt.digits excel a q value key gcc make contents tar ghc cabal clean _make distcc ghc " ++
+    "newtype do MyFile.txt.digits excel a q m c x value key gcc cl os make contents tar ghc cabal clean _make distcc ghc " ++
     ".. /./ /.. ./ // \\ ../ " ++
     "ConstraintKinds GeneralizedNewtypeDeriving DeriveDataTypeable SetConsoleTitle " ++
-    "NoProgress Error " ++
-    ".make/i586-linux-gcc/output _make/.database foo/.. file.src file.out " ++
-    "/usr/special /usr/special/userbinary $CFLAGS -O2 header.h source.c " ++
-    "-threaded -rtsopts -I0 Function extension $OUT $PATH xterm $TERM main opts result flagValues argValues " ++
-    "HEADERS_DIR /path/to/dir CFLAGS "
+    "Data.List System.Directory Development.Shake.FilePath main.m run .rot13 " ++
+    "NoProgress Error src rot13 " ++
+    ".make/i586-linux-gcc/output _make/.database foo/.. file.src file.out build " ++
+    "/usr/special /usr/special/userbinary $CFLAGS %PATH% -O2 -j8 -j -j1 " ++
+    "-threaded -rtsopts -I0 Function extension $OUT $C_LINK_FLAGS $PATH xterm $TERM main opts result flagValues argValues " ++
+    "HEADERS_DIR /path/to/dir CFLAGS let -showincludes -MMD gcc.version linkFlags temp pwd code out err " ++
+    "_metadata/.database _shake _shake/build ./build.sh build.sh build.bat //* [out] manual/examples.zip manual " ++
+    "docs/manual _build _build/run ninja depfile build.ninja "
     = True
 whitelist x = x `elem`
     ["[Foo.hi, Foo.o]"
@@ -186,6 +218,7 @@ whitelist x = x `elem`
     ,"main -j6"
     ,"main clean"
     ,"1m25s (15%)"
+    ,"3m12s (82%)"
     ,"getPkgVersion $ GhcPkgVersion \"shake\""
     ,"# command-name file-name"
     ,"ghc --make MyBuildSystem -rtsopts -with-rtsopts=-I0"
@@ -195,10 +228,37 @@ whitelist x = x `elem`
     ,"# This is my Config file"
     ,"-g -I/path/to/dir -O2"
     ,"main _make/henry.txt"
+    ,"<i>build rules</i>"
+    ,"<i>actions</i>"
+    ,"() <- cmd ..."
+    ,"x <- inputs"
+    ,"shakeFiles=\"_build/\""
+    ,"#include \""
+    ,"pattern *> actions = (pattern ?==) ?> actions" -- because it overlaps
+    ,"buildDir = \"_build\""
+    ,"-MMD -MF"
+    ,"#!/bin/sh"
+    ,"build _build/main.o"
+    ,"build clean"
+    ,"build -j8"
+    ,"runhaskell _build/run"
+    ,"runhaskell _build/run clean"
+    ,"gcc -c main.c -o main.o -MMD -MF main.m"
+    ,"\"_build\" </> x -<.> \"o\""
+    ,"cmd \"gcc -o\" [out] os"
+    ,"rot13 file.txt -o file.rot13"
+    ,"file.rot13"
+    ,"out -<.> \"txt\""
+    ,"[item1,item2,item2]"
+    ,"runhaskell Build.hs"
+    ,"cabal update"
+    ,"cabal install shake"
+    ,"shake -j4"
+    ,"cmd \"gcc -o _make/run _build/main.o _build/constants.o\""
     ]
 
 types = words $
-    "MVar IO Monad Monoid String FilePath Data [String] Eq Typeable Char ExitCode " ++
+    "MVar IO Monad Monoid String FilePath Data Maybe [String] Eq Typeable Char ExitCode " ++
     "Action Resource Assume FilePattern Lint Verbosity Rules Rule CmdOption CmdResult Int Double"
 
 dupes = words "main progressSimple rules"
