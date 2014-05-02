b8b951f07b2715f948371387a5aacd5d2f6a8fba
Neil Mitchell <ndmitchell@gmail.com>
2014-05-02 08:52:59 +0100

    Always do --version 3 times, not just if it fails

--- travis.hs
+++ travis.hs
@@ -11,7 +11,7 @@ main = do
     setCurrentDirectory "ninja"
 
     let ms x = show $ ceiling $ x * 1000
-    retry 3 $ do
+    replicateM 3 $ do
         (ninjaVer, _) <- duration $ cmd "../nin --version"
         (shakeVer, _) <- duration $ cmd "shake --version"
         putStrLn $ "--version for Ninja is " ++ ms ninjaVer ++ ", for Shake is " ++ ms shakeVer
