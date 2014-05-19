751d994d0960a3b231f1e797c38c7d98965d668e
Neil Mitchell <ndmitchell@gmail.com>
2014-05-19 14:45:44 +0100

    Update the docs tests for the Change type

--- Examples/Test/Docs.hs
+++ Examples/Test/Docs.hs
@@ -208,7 +208,7 @@ whitelist x | elem x $ words $
     ".make/i586-linux-gcc/output _make/.database foo/.. file.src file.out build " ++
     "/usr/special /usr/special/userbinary $CFLAGS %PATH% -O2 -j8 -j -j1 " ++
     "-threaded -rtsopts -I0 Function extension $OUT $C_LINK_FLAGS $PATH xterm $TERM main opts result flagValues argValues " ++
-    "HEADERS_DIR /path/to/dir CFLAGS let -showincludes -MMD gcc.version linkFlags temp pwd code out err " ++
+    "HEADERS_DIR /path/to/dir CFLAGS let -showincludes -MMD gcc.version linkFlags temp pwd touch code out err " ++
     "_metadata/.database _shake _shake/build ./build.sh build.sh build.bat //* [out] manual/examples.zip manual " ++
     "docs/manual _build _build/run ninja depfile build.ninja "
     = True
@@ -258,7 +258,7 @@ whitelist x = x `elem`
     ]
 
 types = words $
-    "MVar IO Monad Monoid String FilePath Data Maybe [String] Eq Typeable Char ExitCode " ++
+    "MVar IO Monad Monoid String FilePath Data Maybe [String] Eq Typeable Char ExitCode Change " ++
     "Action Resource Assume FilePattern Lint Verbosity Rules Rule CmdOption CmdResult Int Double"
 
 dupes = words "main progressSimple rules"
