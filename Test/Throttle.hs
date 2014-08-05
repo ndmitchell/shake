aa5b03c6ad9b1b063235f397d81f170c75ad479f
Neil Mitchell <ndmitchell@gmail.com>
2014-08-05 18:24:01 +0100

    Make sure the throttle tests checks when everyone is waiting and nothing has been returned

--- Test/Throttle.hs
+++ Test/Throttle.hs
@@ -12,7 +12,8 @@ main = shaken test $ \args obj -> do
     res <- newThrottle "test" 2 0.2
     want $ map obj ["file1.1","file2.1","file3.2","file4.1","file5.2"]
     obj "*.*" *> \out -> do
-        withResource res (read $ drop 1 $ takeExtension out) $ return ()
+        withResource res (read $ drop 1 $ takeExtension out) $
+            when (takeBaseName out == "file3") $ liftIO $ sleep 0.1
         writeFile' out ""
 
 test build obj = do
@@ -24,5 +25,5 @@ test build obj = do
             (s, _) <- duration $ build []
             -- the 0.1s cap is a guess at an upper bound for how long everything else should take
             -- and should be raised on slower machines
-            assert (s >= 0.6 && s < 0.7) $
+            assert (s >= 0.7 && s < 0.8) $
                 "Bad throttling, expected to take 0.7s + computation time (cap of 0.1s), took " ++ show s ++ "s"
