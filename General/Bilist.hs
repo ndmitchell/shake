e1c4fcfd395b8f175a5b47c1fd1bd699b704b4ce
Neil Mitchell <ndmitchell@gmail.com>
2014-08-05 18:34:28 +0100

    Change the Bilist implementation to the classic queue, probably faster

--- General/Bilist.hs
+++ General/Bilist.hs
@@ -3,43 +3,31 @@ module General.Bilist(
     Bilist, cons, snoc, uncons, toList, isEmpty
     ) where
 
-import Control.Arrow
-import Data.Maybe
 import Data.Monoid
 
 
-newtype Bilist a = Bilist (Maybe (Tree a))
-
-data Tree a = Leaf a | Branch (Tree a) (Tree a)
+data Bilist a = Bilist [a] [a]
 
 toList :: Bilist a -> [a]
-toList (Bilist Nothing) = []
-toList (Bilist (Just x)) = f x []
-    where f (Leaf x) = (x:)
-          f (Branch x y) = f x . f y
+toList (Bilist as bs) = as ++ reverse bs
 
 isEmpty :: Bilist a -> Bool
-isEmpty (Bilist x) = isNothing x
+isEmpty (Bilist as bs) = null as && null bs
 
 instance Eq a => Eq (Bilist a) where
     a == b = toList a == toList b
 
 instance Monoid (Bilist a) where
-    mempty = Bilist Nothing
-    mappend (Bilist Nothing) x = x
-    mappend x (Bilist Nothing) = x
-    mappend (Bilist (Just x)) (Bilist (Just y)) = Bilist $ Just $ Branch x y
-
-one x = Bilist $ Just $ Leaf x
+    mempty = Bilist [] []
+    mappend a b = Bilist (toList a ++ toList b) []
 
 cons :: a -> Bilist a -> Bilist a
-cons x xs = one x `mappend` xs
+cons x (Bilist as bs) = Bilist (x:as) bs
 
 snoc :: Bilist a -> a -> Bilist a
-snoc xs x = xs `mappend` one x
+snoc (Bilist as bs) x = Bilist as (x:bs)
 
 uncons :: Bilist a -> Maybe (a, Bilist a)
-uncons (Bilist Nothing) = Nothing
-uncons (Bilist (Just x)) = Just $ f x
-    where f (Leaf x) = (x, mempty)
-          f (Branch x y) = second (`mappend` Bilist (Just y)) $ f x
+uncons (Bilist [] []) = Nothing
+uncons (Bilist (a:as) bs) = Just (a, Bilist as bs)
+uncons (Bilist [] bs) = uncons $ Bilist (reverse bs) []
