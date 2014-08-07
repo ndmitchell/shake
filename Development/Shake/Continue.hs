bf71bb789189672e45f894a10d86e60a748ab831
Neil Mitchell <ndmitchell@gmail.com>
2014-08-07 07:36:09 +0100

    Delete the unused methods from Continue

--- Development/Shake/Continue.hs
+++ Development/Shake/Continue.hs
@@ -1,24 +1,16 @@
 
 module Development.Shake.Continue(
-    Continue(..), ($$),
+    Continue(..),
     Fence, newFence, signalFence, waitFence, testFence,
-    mendFences,
-    addPoolContinue
     ) where
 
 import Control.Applicative
 import Control.Exception as E
 import Control.Monad
 import Data.IORef
-import Development.Shake.Pool
 
 
 newtype Continue a = Continue {runContinue :: Either SomeException a -> IO ()}
-
-($$) :: Continue b -> (a -> IO b) -> Continue a
-($$) c op = Continue $ \v -> case v of
-    Left e -> runContinue c $ Left e
-    Right v -> runContinue c . Right =<< op v
 
 
 -- | Like a barrier, but based on callbacks
@@ -41,29 +33,3 @@ waitFence (Fence ref) call = join $ atomicModifyIORef ref $ \x -> case x of
 
 testFence :: Fence a -> IO (Maybe (Either SomeException a))
 testFence (Fence x) = either (const Nothing) Just <$> readIORef x
-
-
--- | Left is continue, Right is abort
-mendFences :: [Fence v] -> r -> (r -> v -> Either r r) -> IO (Fence r)
-mendFences deps r op = do
-    res <- newFence
-    if null deps then
-        signalFence res `runContinue` Right r
-     else do
-        ref <- newIORef $ Just (length deps, r)
-        forM_ deps $ \dep ->
-            waitFence dep $ Continue $ \a ->
-                join $ atomicModifyIORef ref $ \v -> case v of
-                    Nothing -> (Nothing, return ())
-                    Just (n, r) -> case a of
-                        Left e -> (Nothing, signalFence res `runContinue` Left e)
-                        Right a -> case op r a of
-                            Left r | n > 1 -> (Just (n-1, r), return ())
-                            r -> (Nothing, signalFence res `runContinue` Right (either id id r))
-    return res
-
-
--- FIXME: Move this to Pool
-addPoolContinue :: Pool -> Continue a -> Continue a
-addPoolContinue pool c = Continue $ \v ->
-    addPool pool $ runContinue c v
