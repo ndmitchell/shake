960cb8fbca9e99fc05d319bac99bd6cac1e4e562
Neil Mitchell <ndmitchell@gmail.com>
2014-08-09 12:51:49 +0100

    #129, use GHC.Conc.getNumProcessors where available

--- General/Base.hs
+++ General/Base.hs
@@ -21,7 +21,6 @@ import Control.Exception as E
 import Control.Monad
 import Data.Char
 import Data.IORef
-import Data.List
 import Data.Maybe
 import Data.Time
 import qualified Data.HashSet as Set
@@ -34,7 +33,11 @@ import System.IO.Unsafe
 import System.Random
 import GHC.IO.Handle(hDuplicate,hDuplicateTo)
 import Development.Shake.Classes
+#if __GLASGOW_HASKELL__ >= 704
+import GHC.Conc(getNumProcessors)
+#else
 import Foreign.C.Types
+#endif
 
 
 ---------------------------------------------------------------------
@@ -199,26 +202,18 @@ isWindows = False
 #endif
 
 
--- Use the underlying GHC function
-foreign import ccall getNumberOfProcessors :: IO CInt
-
-
 {-# NOINLINE getProcessorCount #-}
 getProcessorCount :: IO Int
+
+#if __GLASGOW_HASKELL__ >= 704
+getProcessorCount = let res = unsafePerformIO getNumProcessors in return res
+#else
 -- unsafePefromIO so we cache the result and only compute it once
-getProcessorCount = let res = unsafePerformIO act in return res
-    where
-        act =
-            if rtsSupportsBoundThreads then
-                fromIntegral <$> getNumberOfProcessors
-            else
-                handle (\(_ :: SomeException) -> return 1) $ do
-                    env <- getEnvMaybe "NUMBER_OF_PROCESSORS"
-                    case env of
-                        Just s | [(i,"")] <- reads s -> return i
-                        _ -> do
-                            src <- readFile "/proc/cpuinfo"
-                            return $ length [() | x <- lines src, "processor" `isPrefixOf` x]
+getProcessorCount = let res = unsafePerformIO $ fromIntegral <$> getNumberOfProcessors in return res
+
+-- Use the underlying GHC function
+foreign import ccall getNumberOfProcessors :: IO CInt
+#endif
 
 
 ---------------------------------------------------------------------
