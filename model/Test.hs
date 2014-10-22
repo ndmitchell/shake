66219451c17fdc746238a375b2813b8e0a3f4722
Neil Mitchell <ndmitchell@gmail.com>
2014-10-22 22:18:05 +0100

    Add a model of Shake, plus some properties

---
+++ model/Test.hs
@@ -0,0 +1,95 @@
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
+{-# OPTIONS_GHC -O0 #-} -- doing unsafe things, so be on the safe side
+
+module Test(
+    (~~), (=%=), K, V,
+    Key(..),
+    ValidRule(..), ValidRuleOnce(..),
+    Property
+    ) where
+
+import Control.Monad.State
+import Control.Arrow
+import Control.Applicative
+import Data.Monoid
+import Data.IORef
+import System.IO.Unsafe
+import Util
+import Test.QuickCheck
+
+
+(~~) :: Testable a => String -> a -> (String, Property)
+(~~) a b = (a, property b)
+
+(=%=) :: V -> K -> Bool
+(=%=) (V k1 _) k2 = k1 == k2
+
+newtype K = K Int deriving (Eq,Show)
+
+ks :: [K]
+ks = map K [1..4]
+
+newtype Key = Key K deriving (Show,Arbitrary)
+
+instance Arbitrary K where
+    arbitrary = elements ks
+
+-- Each V has a K it is associated with and a Bool value
+data V = V K Bool deriving (Eq,Show)
+
+data Op = Skip | Not
+
+data Rule = Rule Bool [(K, Op)]
+
+instance Arbitrary Op where
+    arbitrary = elements [Skip,Not]
+
+instance Arbitrary Rule where
+    arbitrary = Rule <$> arbitrary <*> arbitrary
+
+fixRule :: K -> Rule -> Rule
+fixRule (K k) (Rule v ops) = Rule v $ filter (good . fst) ops
+    where good (K x) = x < k
+
+evalRules :: [(K,Rule)] -> (K -> Action K V)
+evalRules rs k | Just (Rule v ops) <- lookup k rs = f v ops
+    where
+        f v [] = Done $ V k v
+        f v ((k,o):xs) = Need k $ \(V k' v') -> case (v',o) of
+            _ | k /= k' -> error $ "Asked for " ++ show k ++ " but got " ++ show k'
+            (True, Skip) -> f v $ drop 1 xs
+            (True, Not ) -> f (not v) xs
+            _              -> f v xs
+
+
+-- | Rule associates each K with a list of dependencies, and an argument for a min operation
+--   so we get mixing
+data ValidRule = ValidRule [(K,Rule)] (K -> Action K V)
+
+data ValidRuleOnce = ValidRuleOnce [(K,Rule)] (K -> Action K V)
+
+instance Show ValidRule where show _ = "ValidRule"
+instance Show ValidRuleOnce where show _ = "ValidRuleOnce"
+
+instance Arbitrary ValidRule where
+    arbitrary = do
+        rs <- forM ks $ \k -> do
+            r <- arbitrary
+            return (k, fixRule k r)
+        return $ ValidRule rs $ evalRules rs
+
+instance Arbitrary ValidRuleOnce where
+    arbitrary = do
+        ValidRule a b <- arbitrary
+        return $ ValidRuleOnce a $ assertOnce b
+
+
+{-# NOINLINE assertOnce #-}
+assertOnce :: (Show k, Eq k) => (k -> v) -> (k -> v)
+assertOnce f = unsafePerformIO $ do
+    ref <- newIORef []
+    return $ \k -> unsafePerformIO $ do
+        ks <- readIORef ref
+        when (k `elem` ks) $ error $ "Key used twice: " ++ show k
+        modifyIORef ref (k:)
+        return $ f k
---
+++ model/Util.hs
@@ -0,0 +1,82 @@
+{-# LANGUAGE GeneralizedNewtypeDeriving, TypeOperators #-}
+
+module Util(
+    Action(..), act, actM, actMT,
+    (|->)(..), -- GHC bug that I can't just export (|->)
+    (!), one,
+    Cache, runCache, cache, askCache,
+    T
+    ) where
+
+import Control.Monad.State
+import Control.Arrow
+import Control.Applicative
+import Data.Monoid
+
+
+newtype T = T Int deriving (Eq,Ord)
+
+instance Enum T where
+    succ (T x) = T (x + 1)
+    toEnum = error "toEnum T"
+    fromEnum = error "fromEnum T"
+
+
+---------------------------------------------------------------------
+-- ACTION
+
+data Action k v = Done v
+                | Need k (v -> Action k v)
+
+act :: (k -> v) -> Action k v -> v
+act f (Done v) = v
+act f (Need k c) = act f $ c $ f k
+
+actM :: Monad m => (k -> m v) -> Action k v -> m v
+actM f (Done v) = return v
+actM f (Need k c) = actM f . c =<< f k
+
+actMT :: Monad m => (k -> m v) -> Action k v -> m (v, [(k,v)])
+actMT f (Done v) = return (v, [])
+actMT f (Need k c) = do
+    v <- f k
+    second ((k,v):) `liftM` actMT f (c v)
+
+
+---------------------------------------------------------------------
+-- ASSOCIATION
+
+data k |-> v = Assoc [(k,v)]
+
+instance Monoid (k |-> v) where
+    mempty = Assoc []
+    mappend (Assoc a) (Assoc b) = Assoc $ a ++ b
+
+(!) :: Eq k => (k |-> v) -> k -> Maybe v
+(!) (Assoc xs) k = lookup k xs
+
+one :: k -> v -> (k |-> v)
+one k v = Assoc [(k,v)]
+
+
+---------------------------------------------------------------------
+-- CACHE
+
+newtype Cache k v a = Cache (State (k |-> v) a)
+    deriving (Functor, Applicative, Monad)
+
+askCache :: Eq k => k -> Cache k v (Maybe v)
+askCache k = Cache $ fmap (! k) get
+
+cache :: Eq k => k -> Cache k v v -> Cache k v v
+cache k act = do
+    v <- askCache k
+    case v of
+        Just v -> return v
+        Nothing -> do
+            v <- act
+            Cache $ modify $ mappend $ one k v
+            return v
+
+runCache :: Cache k v a -> (k |-> v) -> (a, k |-> v)
+runCache (Cache s) = runState s
