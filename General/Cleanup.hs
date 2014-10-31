a3734b2644ba1c2e1e49a64410d91bbc98e3845f
Neil Mitchell <ndmitchell@gmail.com>
2014-10-31 07:40:30 +0000

    Cleanup, ensure the functions are run in the correct order

--- General/Cleanup.hs
+++ General/Cleanup.hs
@@ -7,7 +7,9 @@ module General.Cleanup(
 import Control.Exception as E
 import Control.Monad
 import qualified Data.HashMap.Strict as Map
+import Data.Function
 import Data.IORef
+import Data.List
 
 
 data S = S {unique :: !Int, items :: Map.HashMap Int (IO ())}
@@ -16,12 +18,13 @@ newtype Cleanup = Cleanup (IORef S)
 
 
 -- | Run with some cleanup scope. Regardless of exceptions/threads, all 'addCleanup' actions
---   will be run by the time it exits.
+--   will be run by the time it exits. The 'addCleanup' actions will be run in reverse order.
 withCleanup :: (Cleanup -> IO a) -> IO a
 withCleanup act = do
     ref <- newIORef $ S 0 Map.empty
     act (Cleanup ref) `finally` do
-        join $ atomicModifyIORef ref $ \s -> (s{items=Map.empty}, sequence_ $ Map.elems $ items s)
+        items <- atomicModifyIORef ref $ \s -> (s{items=Map.empty}, items s)
+        mapM_ snd $ sortBy (compare `on` negate . fst) $ Map.toList items
 
 
 -- | Add a cleanup action to a 'Cleanup' scope. If the return action is not run by the time
