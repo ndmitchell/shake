5bb9c038333b46db3636c62c76f239e899ef5d0a
Neil Mitchell <ndmitchell@gmail.com>
2014-11-17 17:34:14 +0000

    #182, use the js-jquery and js-flot libraries, avoid shipping Javascript sources with my tarball (yay!)

--- src/Test/Self.hs
+++ src/Test/Self.hs
@@ -84,4 +84,4 @@ cabalBuildDepends _ = packages ++ ["unix" | os /= "mingw32"]
 
 packages = words $
     "base transformers binary unordered-containers hashable time old-time bytestring " ++
-    "filepath directory process deepseq random utf8-string extra"
+    "filepath directory process deepseq random utf8-string extra js-jquery js-flot"
