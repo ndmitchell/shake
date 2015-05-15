419e48e92f0f3bee3bae3305c91ea63808423b1c
Neil Mitchell <ndmitchell@gmail.com>
2015-05-15 13:51:06 +0100

    #245, abstract out a hasTracker function

--- src/Test/Type.hs
+++ src/Test/Type.hs
@@ -7,6 +7,7 @@ import General.String
 import Development.Shake.FileInfo
 import Development.Shake.FilePath
 
+import Control.Applicative
 import Control.Exception.Extra hiding (assert)
 import Control.Monad.Extra
 import Data.List
@@ -19,6 +20,7 @@ import System.Console.GetOpt
 import System.IO
 import System.Time.Extra
 import System.Info.Extra(isWindows)
+import Prelude
 
 
 shaken
@@ -61,17 +63,18 @@ shaken test rules sleeper = do
 
         args -> do
             let (_,files,_) = getOpt Permute [] args
-            tracker <- if isWindows
-                       then findExecutable "tracker.exe"
-                       else lookupEnv "FSAT"
+            tracker <- hasTracker
             withArgs (args \\ files) $
                 shakeWithClean
                     (removeDirectoryRecursive out)
                     (shakeOptions{ shakeFiles=out
                                  , shakeReport=["output/" ++ name ++ "/report.html"]
-                                 , shakeLint= Just $ if isJust tracker then LintTracker else LintBasic
+                                 , shakeLint= Just $ if tracker then LintTracker else LintBasic
                                  })
                     (rules files obj)
+
+hasTracker :: IO Bool
+hasTracker = isJust <$> if isWindows then findExecutable "tracker.exe" else lookupEnv "FSAT"
 
 
 shakeWithClean :: IO () -> ShakeOptions -> Rules () -> IO ()
