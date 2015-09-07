a12075760a5dd6f5ab11db6049f1700e754745e8
Neil Mitchell <ndmitchell@gmail.com>
2015-09-07 17:56:04 +0100

    Rename FilePattern2 to FilePattern, FilePattern to FilePatternOld

--- src/Development/Shake/FilePattern.hs
+++ src/Development/Shake/FilePattern.hs
@@ -10,197 +10,113 @@ module Development.Shake.FilePattern(
     -- * Accelerated searching
     directories,
     -- * Testing only
-    directories1
+    directories1,
+    internalTest
     ) where
 
-import System.FilePath(isPathSeparator, pathSeparators, pathSeparator)
+import Development.Shake.FilePatternOld hiding ((?==))
+import System.FilePath(isPathSeparator)
 import Data.List.Extra
-import Data.Tuple.Extra
+import Control.Applicative
+import Control.Monad
+import Prelude
 
 
----------------------------------------------------------------------
--- BASIC FILE PATTERN MATCHING
-
--- | A type synonym for file patterns, containing @\/\/@ and @*@. For the syntax
---   and semantics of 'FilePattern' see '?=='.
---
---   Most 'normaliseEx'd 'FilePath' values are suitable as 'FilePattern' values which match
---   only that specific file. On Windows @\\@ is treated as equivalent to @\/@.
---
---   You can write 'FilePattern' values as a literal string, or build them
---   up using the operators 'Development.Shake.FilePath.<.>', 'Development.Shake.FilePath.</>'
---   and 'Development.Shake.<//>'. However, beware that:
---
--- * On Windows, use 'Development.Shake.FilePath.<.>' from "Development.Shake.FilePath" instead of from
---   "System.FilePath" - otherwise @\"\/\/*\" \<.\> exe@ results in @\"\/\/*\\\\.exe\"@.
---
--- * If the second argument of 'Development.Shake.FilePath.</>' has a leading path separator (namely @\/@)
---   then the second argument will be returned.
-type FilePattern = String
+data Pat = Lit String -- foo
+         | Star   -- /*/
+         | Skip -- //
+         | Skip1 -- //, but must be at least 1 element
+         | Stars String [String] String -- *foo*, prefix (fixed), infix floaters, suffix
+                                        -- e.g. *foo*bar = Stars "" ["foo"] "bar"
+            deriving (Show,Eq)
 
 
-data Lexeme = Star | SlashSlash | Char Char deriving (Show, Eq)
-
-isDull (Char x) = not $ isPathSeparator x; isDull _ = False
-fromChar (Char x) = x
-
-
-data Regex = Lit [Char] | Not [Char] | Any
-           | Start | End
-           | Bracket Regex
-           | Or Regex Regex | Concat Regex Regex
-           | Repeat Regex | Empty | None
-             deriving Show
-
-type SString = (Bool, String) -- fst is True if at the start of the string
-
+data Lexeme = Str String | Slash | SlashSlash
 
 lexer :: FilePattern -> [Lexeme]
-lexer ('*':xs) = Star : lexer xs
-lexer (s1:s2:xs) | isPathSeparator s1 && isPathSeparator s2 = SlashSlash : lexer xs
-lexer (x:xs) = Char x : lexer xs
-lexer [] = []
+lexer "" = []
+lexer (x1:x2:xs) | isPathSeparator x1, isPathSeparator x2 = SlashSlash : lexer xs
+lexer (x1:xs) | isPathSeparator x1 = Slash : lexer xs
+lexer xs = Str a : lexer b
+    where (a,b) = break isPathSeparator xs
 
 
-pattern :: [Lexeme] -> Regex
-pattern = Concat Start . foldr Concat End . f Start
+-- | Parse a FilePattern. All optimisations I can think of are invalid because they change the extracted expressions.
+parse :: FilePattern -> [Pat]
+parse = f False True . lexer
     where
-        f start (Star:xs) = Bracket (Repeat $ Not pathSeparators) : f None xs
-        f start (SlashSlash:SlashSlash:xs) = Bracket Empty : f start (SlashSlash:xs)
-        f start (SlashSlash:Char x:xs) | isPathSeparator x =
-            Bracket (((start `Or` Lit pathSeparators) `Concat` Repeat Any) `Or` Empty) `Concat` Lit pathSeparators : f None xs
-        f start (SlashSlash:xs) = Bracket (Or (s `Concat` Repeat Any `Concat` s) (Lit pathSeparators)) : f start xs
-            where s = start `Or` (if all (== SlashSlash) xs then End else None) `Or` Lit pathSeparators
-        f start (Char x:xs) = Lit (if isPathSeparator x then pathSeparators else [x]) : f None xs
-        f start [] = []
+        -- str = I have ever seen a Str go past (equivalent to "can I be satisfied by no paths")
+        -- slash = I am either at the start, or my previous character was Slash
+        f str slash [] = [Lit "" | slash]
+        f str slash (Str x:xs) = parseLit x : f True False xs
+        f str slash (SlashSlash:Slash:xs) | not str = Skip1 : f str True xs
+        f str slash (SlashSlash:xs) = Skip : f str False xs
+        f str slash (Slash:xs) = [Lit "" | not str] ++ f str True xs
 
 
--- | Return is (brackets, matched, rest)
-match :: Regex -> SString -> [([String], String, SString)]
-match (Lit l) (_, x:xs) | x `elem` l = [([], [x], (False, xs))]
-match (Not l) (_, x:xs) | x `notElem` l = [([], [x], (False, xs))]
-match Any (_, x:xs) = [([], [x], (False, xs))]
-match Start (True, xs) = [([], [], (True, xs))]
-match End (s, []) = [([], [], (s, []))]
-match (Bracket r) xs = [(a ++ [b], b, c) | (a,b,c) <- match r xs]
-match (Or r1 r2) xs = match r1 xs ++ match r2 xs
-match (Concat r1 r2) xs = [(a1++a2,b1++b2,c2) | (a1,b1,c1) <- match r1 xs, (a2,b2,c2) <- match r2 c1]
-match (Repeat r) xs = match (Empty `Or` Concat r (Repeat r)) xs
-match Empty xs = [([], "", xs)]
+parseLit :: String -> Pat
+parseLit "*" = Star
+parseLit x = case split (== '*') x of
+    [x] -> Lit x
+    pre:xs | Just (mid,post) <- unsnoc xs -> Stars pre mid post
+
+
+internalTest :: IO ()
+internalTest = do
+    let x # y = when (parse x /= y) $ fail $ show ("FilePattern.internalTest",x,parse x,y)
+    "" # [Lit ""]
+    "/" # [Lit "",Lit ""]
+    "x/" # [Lit "x",Lit ""]
+    "/x" # [Lit "",Lit "x"]
+    "x/y" # [Lit "x",Lit "y"]
+    "//" # [Skip]
+    "//x" # [Skip, Lit "x"]
+    "x//" # [Lit "x", Skip]
+    "x//y" # [Lit "x",Skip, Lit "y"]
+    "///" # [Skip1, Lit ""]
+    "///x" # [Skip1, Lit "x"]
+    "x///" # [Lit "x", Skip, Lit ""]
+    "x///y" # [Lit "x",Skip, Lit "y"]
+    "////" # [Skip, Skip]
+    "////x" # [Skip, Skip, Lit "x"]
+    "x////" # [Lit "x", Skip, Skip]
+    "x////y" # [Lit "x",Skip, Skip, Lit "y"]
+
+
+-- | Optimisations that may change the matched expressions
+optimise :: [Pat] -> [Pat]
+optimise (Skip:Skip:xs) = optimise $ Skip:xs
+optimise (Skip:Star:xs) = optimise $ Skip1:xs
+optimise (Star:Skip:xs) = optimise $ Skip1:xs
+optimise (x:xs) = x : optimise xs
+optimise [] =[]
+
+
+match :: [Pat] -> [String] -> [[String]]
+match (Skip:xs) (y:ys) = map ([]:) (match xs (y:ys)) ++ [(if null r then y else y ++ "/" ++ r):rs | r:rs <- match (Skip:xs) ys]
+match (Skip1:xs) (y:ys) = [(if null r then y else y ++ "/" ++ r):rs | r:rs <- match (Skip:xs) ys]
+match (Skip:xs) [] = map ([]:) $ match xs []
+match (Star:xs) (y:ys) = map (y:) $ match xs ys
+match (Lit x:xs) (y:ys) | x == y = match xs ys
+match (x@Stars{}:xs) (y:ys) | Just rs <- matchStars x y = map (rs++) $ match xs ys
+match [] [] = [[]]
 match _ _ = []
 
+-- Only return the first (all patterns left-most) valid star matching
+matchStars :: Pat -> String -> Maybe [String]
+matchStars (Stars pre mid post) x = do
+    x <- stripPrefix pre x
+    x <- if null post then Just x else stripSuffix post x
+    stripInfixes mid x
+    where
+        stripInfixes [] x = Just [x]
+        stripInfixes (m:ms) x = do
+            (a,x) <- stripInfix m x
+            (a:) <$> stripInfixes ms x
 
--- | Match a 'FilePattern' against a 'FilePath', There are only two special forms:
---
--- * @*@ matches an entire path component, excluding any separators.
---
--- * @\/\/@ matches an arbitrary number of path components.
---
---   Some examples:
---
--- * @test.c@ matches @test.c@ and nothing else.
---
--- * @*.c@ matches all @.c@ files in the current directory, so @file.c@ matches,
---   but @file.h@ and @dir\/file.c@ don't.
---
--- * @\/\/*.c@ matches all @.c@ files in the current directory or its subdirectories,
---   so @file.c@, @dir\/file.c@ and @dir1\/dir2\/file.c@ all match, but @file.h@ and
---   @dir\/file.h@ don't.
---
--- * @dir\/*\/*@ matches all files one level below @dir@, so @dir\/one\/file.c@ and
---   @dir\/two\/file.h@ match, but @file.c@, @one\/dir\/file.c@, @dir\/file.h@
---   and @dir\/one\/two\/file.c@ don't.
---
---   Patterns with constructs such as @foo\/..\/bar@ will never match
---   normalised 'FilePath' values, so are unlikely to be correct.
+
 (?==) :: FilePattern -> FilePath -> Bool
-(?==) [s1,s2,'*'] | isPathSeparator s1 && isPathSeparator s2 = const True
-(?==) p = \x -> not $ null $ match pat (True, x)
-    where pat = pattern $ lexer p
-
-infixr 5 <//>
-
--- | Join two 'FilePattern' values by inserting two @\/@ characters between them.
---   Will first remove any trailing path separators on the first argument, and any leading
---   separators on the second.
---
--- > "dir" <//> "*" == "dir//*"
-(<//>) :: FilePattern -> FilePattern -> FilePattern
-a <//> b = dropWhileEnd isPathSeparator a ++ "//" ++ dropWhile isPathSeparator b
-
-
----------------------------------------------------------------------
--- DIRECTORY PATTERNS
-
--- | Given a pattern, return the directory that requires searching,
---   with 'True' if it requires a recursive search. Must be conservative.
---   Examples:
---
--- > directories1 "*.xml" == ("",False)
--- > directories1 "//*.xml" == ("",True)
--- > directories1 "foo//*.xml" == ("foo",True)
--- > directories1 "foo/bar/*.xml" == ("foo/bar",False)
--- > directories1 "*/bar/*.xml" == ("",True)
-directories1 :: FilePattern -> (FilePath, Bool)
-directories1 = first (intercalate [pathSeparator]) . f . lexer
-    where
-        f xs | (a@(_:_),b:bs) <- span isDull xs, b `elem` (SlashSlash:map Char pathSeparators) =
-                if b == SlashSlash then ([map fromChar a],True) else first (map fromChar a:) $ f bs
-             | all (\x -> isDull x || x == Star) xs = ([],False)
-             | otherwise = ([], True)
-
-
--- | Given a set of patterns, produce a set of directories that require searching,
---   with 'True' if it requires a recursive search. Must be conservative. Examples:
---
--- > directories ["*.xml","//*.c"] == [("",True)]
--- > directories ["bar/*.xml","baz//*.c"] == [("bar",False),("baz",True)]
--- > directories ["bar/*.xml","baz//*.c"] == [("bar",False),("baz",True)]
-directories :: [FilePattern] -> [(FilePath,Bool)]
-directories ps = foldl f xs xs
-    where
-        xs = nubOrd $ map directories1 ps
-
-        -- Eliminate anything which is a strict subset
-        f xs (x,True) = filter (\y -> not $ (x,False) == y || x `isPrefixSlashOf` fst y) xs
-        f xs _ = xs
-
-        isPrefixSlashOf x (stripPrefix x -> Just (s1:_)) = isPathSeparator s1
-        isPrefixSlashOf _ _ = False
-
-
----------------------------------------------------------------------
--- MULTIPATTERN COMPATIBLE SUBSTITUTIONS
-
-specials :: FilePattern -> String
-specials ('*':xs) = '*' : specials xs
-specials (x1:x2:xs) | isPathSeparator x1, isPathSeparator x2 = '/':'/': specials xs
-specials (x:xs) = specials xs
-specials [] = []
-
--- | Is the pattern free from any * and //.
-simple :: FilePattern -> Bool
-simple = null . specials
-
--- | Do they have the same * and // counts in the same order
-compatible :: [FilePattern] -> Bool
-compatible [] = True
-compatible (x:xs) = all ((==) (specials x) . specials) xs
-
-
--- | Extract the items that match the wildcards. The pair must match with '?=='.
-extract :: FilePattern -> FilePath -> [String]
-extract p x = ms
-    where (ms,_,_):_ = match (pattern $ lexer p) (True,x)
-
-
--- | Given the result of 'extract', substitute it back in to a 'compatible' pattern.
---
--- > p '?==' x ==> substitute (extract p x) p == x
-substitute :: [String] -> FilePattern -> FilePath
-substitute ms p = f ms (lexer p)
-    where
-        f ms (Char p:ps) = p : f ms ps
-        f (m:ms) (_:ps) = m ++ f ms ps
-        f [] [] = []
-        f _ _ = error $ "Substitution failed into pattern " ++ show p ++ " with " ++ show (length ms) ++ " matches, namely " ++ show ms
+(?==) p = case optimise $ parse p of
+    [Skip] -> const True
+    [Skip1] -> const True
+    p -> not . null . match p . split isPathSeparator
