fd7aa12a5dad3a0a34d94cf10d5d09deb1e24e9a
Neil Mitchell <ndmitchell@gmail.com>
2015-10-04 21:41:38 +0100

    #309, add debug messages to try and figure out how the test fails

--- src/Test/FileLock.hs
+++ src/Test/FileLock.hs
@@ -6,6 +6,7 @@ import Control.Concurrent.Extra
 import Control.Exception.Extra
 import Control.Monad
 import Data.Either.Extra
+import System.Time.Extra
 import Test.Type
 
 
@@ -15,10 +16,17 @@ main = shaken test $ \args obj -> do
 
 test build obj = do
     -- check it fails exactly once
-    a <- onceFork $ build []
-    b <- onceFork $ build []
+    time <- offsetTime
+    lock <- newLock
+    let out msg = do t <- time; withLock lock $ print (t, msg)
+    out "before onceFork"
+    a <- onceFork $ do out "a1"; build []; out "a2"
+    b <- onceFork $ do out "b1"; build []; out "b2"
+    out "after onceFork"
     a <- try_ a
+    out "after try a"
     b <- try_ b
+    out "after try b"
     when (length (filter isLeft [a,b]) /= 1) $
         error $ "Expected one success and one failure, got " ++ show [a,b]
     -- check it succeeds after the lock has been held
