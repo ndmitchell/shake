621d174d0c26376b3c734bca1ab59d8f0e92422d
Neil Mitchell <ndmitchell@gmail.com>
2015-11-23 07:34:00 +0000

    #291, clean up the resource test a bit

--- src/Test/Resources.hs
+++ src/Test/Resources.hs
@@ -8,26 +8,30 @@ import Data.IORef
 
 
 main extra = do
-    let cap = 2
-
     ref <- newIORef 0
     flip (shaken test) extra $ \args obj -> do
-        want $ map obj ["file1.txt","file2.txt","file3.txt","file4.txt"]
-        res <- newResource "test" cap
-        res2 <- newResource "test" cap
-        unless (res < res2 || res2 < res) $ error "Resources should have a good ordering"
+        want args
+
+        let cap = 2
+        r1_cap <- newResource "test" cap
+        r2_cap <- newResource "test" cap
+        unless (r1_cap < r2_cap || r2_cap < r1_cap) $ error "Resources should have a good ordering"
+
+        phony "cap" $
+            need $ map obj ["file1.txt","file2.txt","file3.txt","file4.txt"]
         obj "*.txt" %> \out ->
-            withResource res 1 $ do
+            withResource r1_cap 1 $ do
                 old <- liftIO $ atomicModifyIORef ref $ \i -> (i+1,i)
                 when (old >= cap) $ error "Too many resources in use at one time"
                 liftIO $ sleep 0.1
                 liftIO $ atomicModifyIORef ref $ \i -> (i-1,i)
                 writeFile' out ""
 
+
 test build obj = do
     build ["clean"]
-    build ["-j2"]
+    build ["-j2","cap"]
     build ["clean"]
-    build ["-j4"]
+    build ["-j4","cap"]
     build ["clean"]
-    build ["-j10"]
+    build ["-j10","cap"]
