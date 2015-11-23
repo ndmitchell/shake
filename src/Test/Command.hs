e596fe6937b450180a556f387bcebef5bc2428d7
Neil Mitchell <ndmitchell@gmail.com>
2015-11-23 14:46:18 +0000

    Fix up the test on Linux, which escapes with ' by default

--- src/Test/Command.hs
+++ src/Test/Command.hs
@@ -100,7 +100,7 @@ main = shaken test $ \args obj -> do
             Stdout out <- cmd helper Shell "\"oSPACE 2\""
             liftIO $ out === "SPACE 2\n"
         (Stdout (), CmdLine x) <- cmd helper ["oSPACE 3","oDIRECT"]
-        unless (" \"oSPACE 3\" oDIRECT" `isSuffixOf` x) $
+        unless (" \"oSPACE 3\" oDIRECT" `isSuffixOf` replace "\'" "\"" x) $
             fail $ "Invalid CmdLine, " ++ x
 
     "path" !> do
