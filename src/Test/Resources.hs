24a72eae15daed51c8cdc30e86c30b822930a39f
Neil Mitchell <ndmitchell@gmail.com>
2015-11-23 07:55:28 +0000

    #291, add a test that resources schedule properly

--- src/Test/Resources.hs
+++ src/Test/Resources.hs
@@ -9,6 +9,7 @@ import Data.IORef
 
 main extra = do
     inside <- newIORef 0
+    done <- newIORef 0
     flip (shaken test) extra $ \args obj -> do
         want args
 
@@ -27,8 +28,24 @@ main extra = do
                 liftIO $ atomicModifyIORef inside $ \i -> (i-1,i)
                 writeFile' out ""
 
+        lock <- newResource "lock" 1
+        phony "schedule" $ do
+            need $ map obj $ "lock1":"done":["free" ++ show i | i <- [1..10]] ++ ["lock2"]
+        obj "done" %> \out -> do
+            need [obj "lock1",obj "lock2"]
+            done <- liftIO $ readIORef done
+            when (done < 10) $ error "Not all managed to schedule while waiting"
+            writeFile' out ""
+        obj "lock*" %> \out -> do
+            withResource lock 1 $ liftIO $ sleep 0.5
+            writeFile' out ""
+        obj "free*" %> \out -> do
+            liftIO $ atomicModifyIORef done $ \i -> (i+1,())
+            writeFile' out ""
+
 
 test build obj = do
     build ["-j2","cap","--clean"]
     build ["-j4","cap","--clean"]
     build ["-j10","cap","--clean"]
+    build ["-j2","schedule","--clean"]
