1e10c7e7393c43a7de72e6e2bedd79b6358e6273
Neil Mitchell <ndmitchell@gmail.com>
2015-11-23 19:01:08 +0000

    Make the resources test more relocatable, use prefixes for each separate test

--- src/Test/Resources.hs
+++ src/Test/Resources.hs
@@ -2,44 +2,48 @@
 module Test.Resources(main) where
 
 import Development.Shake
+import Development.Shake.Core(rulesIO)
 import Test.Type
 import Control.Monad
 import Data.IORef
 
 
-main extra = do
-    inside <- newIORef 0
-    done <- newIORef 0
-    flip (shaken test) extra $ \args obj -> do
-        want args
+main = shaken test $ \args obj -> do
+    -- test I have good orderings
+    do
+        r1 <- newResource "test" 2
+        r2 <- newResource "test" 2
+        unless (r1 < r2 || r2 < r1) $ error "Resources should have a good ordering"
 
+    -- test you are capped to a maximum value
+    do
         let cap = 2
-        r1_cap <- newResource "test" cap
-        r2_cap <- newResource "test" cap
-        unless (r1_cap < r2_cap || r2_cap < r1_cap) $ error "Resources should have a good ordering"
-
-        phony "cap" $
-            need $ map obj ["file1.txt","file2.txt","file3.txt","file4.txt"]
-        obj "*.txt" %> \out ->
-            withResource r1_cap 1 $ do
+        inside <- rulesIO $ newIORef 0
+        resource <- newResource "test" cap
+        phony "cap" $ need [obj $ "c_file" ++ show i ++ ".txt" | i <- [1..4]]
+        obj "c_*.txt" %> \out ->
+            withResource resource 1 $ do
                 old <- liftIO $ atomicModifyIORef inside $ \i -> (i+1,i)
                 when (old >= cap) $ error "Too many resources in use at one time"
                 liftIO $ sleep 0.1
                 liftIO $ atomicModifyIORef inside $ \i -> (i-1,i)
                 writeFile' out ""
 
+    -- test things can still run while you are blocked on a resource
+    do
+        done <- rulesIO $ newIORef 0
         lock <- newResource "lock" 1
         phony "schedule" $ do
-            need $ map obj $ "lock1":"done":["free" ++ show i | i <- [1..10]] ++ ["lock2"]
-        obj "done" %> \out -> do
-            need [obj "lock1",obj "lock2"]
+            need $ map (\x -> obj $ "s_" ++ x) $ "lock1":"done":["free" ++ show i | i <- [1..10]] ++ ["lock2"]
+        obj "s_done" %> \out -> do
+            need [obj "s_lock1",obj "s_lock2"]
             done <- liftIO $ readIORef done
             when (done < 10) $ error "Not all managed to schedule while waiting"
             writeFile' out ""
-        obj "lock*" %> \out -> do
+        obj "s_lock*" %> \out -> do
             withResource lock 1 $ liftIO $ sleep 0.5
             writeFile' out ""
-        obj "free*" %> \out -> do
+        obj "s_free*" %> \out -> do
             liftIO $ atomicModifyIORef done $ \i -> (i+1,())
             writeFile' out ""
 
