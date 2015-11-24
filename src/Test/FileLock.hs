46b7533f468015b22fdd2fa13415e402def39625
Neil Mitchell <ndmitchell@gmail.com>
2015-11-24 19:08:09 +0000

    Revert "#309, more diagnostics"
    This reverts commit a146ec6642bf85ad504f008d11cd3b99f01ac2c2.
    
    Conflicts:
    	src/Test/FileLock.hs

--- src/Test/FileLock.hs
+++ src/Test/FileLock.hs
@@ -10,11 +10,8 @@ import System.Time.Extra
 import Test.Type
 
 
-main = shaken test $ \args obj ->
-    action $ do
-        putNormal "Starting sleep"
-        liftIO $ sleep 5
-        putNormal "Finished sleep"
+main = shaken test $ \args obj -> do
+    action $ liftIO $ sleep 5
 
 
 test build obj = do
@@ -23,8 +20,8 @@ test build obj = do
     lock <- newLock
     let out msg = do t <- time; withLock lock $ print (t, msg)
     out "before onceFork"
-    a <- onceFork $ do out "a1"; build ["-VVV"]; out "a2"
-    b <- onceFork $ do out "b1"; build ["-VVV"]; out "b2"
+    a <- onceFork $ do out "a1"; build []; out "a2"
+    b <- onceFork $ do out "b1"; build []; out "b2"
     out "after onceFork"
     a <- try_ a
     out "after try a"
