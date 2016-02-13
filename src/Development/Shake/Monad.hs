025969670c9136409dd02cfd8a3e115470face50
Neil Mitchell <ndmitchell@gmail.com>
2016-02-13 08:40:57 +0000

    #415, use the catch safety trick suggested by Simon Peyton Jones

--- src/Development/Shake/Monad.hs
+++ src/Development/Shake/Monad.hs
@@ -15,6 +15,7 @@ import Control.Monad.Trans.Reader
 import Data.IORef
 import Control.Applicative
 import Control.Monad
+import GHC.Exts(lazy)
 import Prelude
 
 
@@ -32,7 +33,7 @@ type Capture a = (a -> IO ()) -> IO ()
 
 -- See https://ghc.haskell.org/trac/ghc/ticket/11555
 catchSafe :: IO a -> (SomeException -> IO a) -> IO a
-catchSafe a b = join (evaluate a) `catch_` b
+catchSafe a b = lazy a `catch_` b
 
 -- | Run and then call a continuation.
 runRAW :: ro -> rw -> RAW ro rw a -> Capture (Either SomeException a)
