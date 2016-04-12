e5159c1c70122e019e5d47ba35231f243f77a110
Neil Mitchell <ndmitchell@gmail.com>
2016-04-12 21:13:00 +0100

    Make sure Travis prints out what it is doing before each command line

--- travis.hs
+++ travis.hs
@@ -15,53 +15,54 @@ requiresShake = words "ghc-make shake-language-c"
 
 ms x = show $ ceiling $ x * 1000
 
+cmd :: String -> IO Seconds
+cmd x = do
+    putStrLn $ "$ " ++ x
+    (t, _) <- duration $ system_ x
+    putStrLn $ "Took " ++ showDuration t
+    return t
+
 main = do
     args <- getArgs
     unless (null args) $ error "Terminating early"
 
     -- grab ninja
-    system_ "git clone https://github.com/martine/ninja"
-    system_ "cd ninja && ./configure.py --bootstrap"
+    cmd "git clone https://github.com/martine/ninja"
+    cmd "cd ninja && ./configure.py --bootstrap"
     copyFile "ninja/ninja" "nin"
 
     withCurrentDirectory "ninja" $ do
 
         replicateM_ 3 $ do
-            (ninjaVer, _) <- duration $ system_ "../nin --version"
-            print "before running shake --version --verbose"
-            (shakeVer, _) <- duration $ system_ "shake --version --verbose"
-            print "before running shake --version --verbose"
-            (shakeVer, _) <- duration $ system_ "shake --version --verbose"
-            print "before running shake --version --quiet"
-            (shakeVer, _) <- duration $ system_ "shake --version --quiet"
-            print "before running shake --version"
-            (shakeVer, _) <- duration $ system_ "shake --version"
-            print "before running shake --version"
-            (shakeVer, _) <- duration $ system_ "shake --version"
-            print "after running shake --version"
+            ninjaVer <- cmd "../nin --version"
+            shakeVer <- cmd "shake --version --verbose"
+            shakeVer <- cmd "shake --version --verbose"
+            shakeVer <- cmd "shake --version --quiet"
+            shakeVer <- cmd "shake --version"
+            shakeVer <- cmd "shake --version"
             putStrLn $ "--version for Ninja is " ++ ms ninjaVer ++ ", for Shake is " ++ ms shakeVer
 
-        system_ "shake --version; shake -C does_not_exist; echo end" -- check for #22
+        cmd "shake --version; shake -C does_not_exist; echo end" -- check for #22
 
         retry 3 $ do
 
             -- time Ninja
-            system_ "../nin -t clean"
-            (ninjaFull, _) <- duration $ system_ "../nin -j3 -d stats"
+            cmd "../nin -t clean"
+            ninjaFull <- cmd "../nin -j3 -d stats"
             ninjaProfile "build/.ninja_log"
             putStrLn =<< readFile "build/.ninja_log"
-            (ninjaZero, _) <- duration $ system_ "../nin -j3 -d stats"
+            ninjaZero <- cmd "../nin -j3 -d stats"
 
             -- time Shake
-            system_ "../nin -t clean"
-            (shakeFull, _) <- duration $ system_ "shake -j3 --quiet --timings"
-            system_ "shake --no-build --report=-"
-            (shakeZero, _) <- duration $ system_ "shake -j3 --quiet --timings"
+            cmd "../nin -t clean"
+            shakeFull <- cmd "shake -j3 --quiet --timings"
+            cmd "shake --no-build --report=-"
+            shakeZero <- cmd "shake -j3 --quiet --timings"
 
             -- Diagnostics
-            system_ "ls -l .shake* build/.ninja*"
-            system_ "shake -VV"
-            (shakeNone, _) <- duration $ system_ "shake --always-make --skip-commands --timings"
+            cmd "ls -l .shake* build/.ninja*"
+            cmd "shake -VV"
+            shakeNone <- cmd "shake --always-make --skip-commands --timings"
             putStrLn $ "--always-make --skip-commands took " ++ ms shakeNone
 
             putStrLn $ "Ninja was " ++ ms ninjaFull ++ " then " ++ ms ninjaZero
@@ -75,7 +76,7 @@ main = do
 
     createDirectoryIfMissing True "temp"
     withCurrentDirectory "temp" $
-        system_ "shake --demo --keep-going"
+        cmd "shake --demo --keep-going"
 
     ghcver <- lookupEnv "GHCVER"
     when (ghcver >= Just "7.6" && ghcver /= Just "head") $ do
@@ -83,7 +84,7 @@ main = do
             src <- readFile "shake.cabal"
             return $ head [dropWhile isSpace x | x <- lines src, Just x <- [stripPrefix "version:" x]]
         forM_ requiresShake $ \x ->
-            retry 3 $ system_ $ "cabal install " ++ x ++ " --constraint=shake==" ++ ver
+            retry 3 $ cmd $ "cabal install " ++ x ++ " --constraint=shake==" ++ ver
 
 ninjaProfile :: FilePath -> IO ()
 ninjaProfile src = do
