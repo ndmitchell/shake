4650cbea5124c74811d06c37a5d27f7b573107d3
Neil Mitchell <ndmitchell@gmail.com>
2016-06-12 18:06:25 +0100

    Delete defaultRule

--- src/Development/Shake/Rule.hs
+++ src/Development/Shake/Rule.hs
@@ -3,20 +3,8 @@
 --   Most users will find the built-in set of rules sufficient.
 module Development.Shake.Rule(
     Rule(..), EqualCost(..), rule, apply, apply1,
-    trackUse, trackChange, trackAllow,
-    -- * Deprecated
-    defaultRule
+    trackUse, trackChange, trackAllow
     ) where
 
 import Development.Shake.Core
 import Development.Shake.Types
-
-{-# DEPRECATED defaultRule "Use 'rule' with 'priority' 0" #-}
-
--- | A deprecated way of defining a low priority rule. Defined as:
---
--- @
--- defaultRule = 'priority' 0 . 'rule'
--- @
-defaultRule :: Rule key value => (key -> Maybe (Action value)) -> Rules ()
-defaultRule = priority 0 . rule
