219b4e807aaeb1dce352b98d76c135a64f270cc0
Neil Mitchell <ndmitchell@gmail.com>
2016-06-30 20:56:41 +0100

    Track the number that are used as well, allow size and sizeUpperBound

--- src/General/Ids.hs
+++ src/General/Ids.hs
@@ -1,8 +1,8 @@
-{-# LANGUAGE RecordWildCards #-}
+{-# LANGUAGE RecordWildCards, BangPatterns, GADTs #-}
 
 module General.Ids(
     Ids, Id,
-    empty, insert, lookup, null
+    empty, insert, lookup, null, size, sizeUpperBound
     ) where
 
 import Data.IORef.Extra
@@ -18,6 +18,7 @@ data Ids a = Ids (IORef (S a))
 
 data S a = S
     {capacity :: {-# UNPACK #-} !Int -- ^ Number of entries in values, initially 0
+    ,used :: {-# UNPACK #-} !Int -- ^ Capacity that has been used, assuming no gaps from index 0, initially 0
     ,values :: {-# UNPACK #-} !(MutableArray RealWorld (Maybe a))
     }
 
@@ -26,29 +27,45 @@ empty :: IO (Ids a)
 empty = do
     -- important to start at capacity == 0 so I can implement null cheaply
     let capacity = 0
+    let used = 0
     values <- newArray capacity Nothing
     Ids <$> newIORef S{..}
 
 
-null :: Ids a -> IO Bool
-null (Ids ref) = do
+sizeUpperBound :: Ids a -> IO Int
+sizeUpperBound (Ids ref) = do
     S{..} <- readIORef ref
-    -- safe because of empty
-    return $ capacity == 0
+    return used
+
+
+size :: Ids a -> IO Int
+size (Ids ref) = do
+    S{..} <- readIORef ref
+    let go !acc i
+            | i < 0 = return acc
+            | otherwise = do
+                v <- readArray values i
+                if isJust v then go (acc+1) (i-1) else go acc (i-1)
+    go 0 (used-1)
+
+
+null :: Ids a -> IO Bool
+null ids = (== 0) <$> sizeUpperBound ids
 
 
 insert :: Id -> a -> Ids a -> IO ()
 insert (Id i) v (Ids ref) = do
     S{..} <- readIORef ref
     let ii = fromIntegral i
-    if ii < capacity then
+    if ii < capacity then do
         writeArray values ii $ Just v
+        when (ii > used) $ writeIORef' ref S{used=ii,..}
      else do
         c2 <- return $ max (capacity * 2) (ii + 10000)
         v2 <- newArray capacity Nothing
         copyMutableArray v2 0 values 0 capacity
         writeArray values ii $ Just v
-        writeIORef' ref $ S c2 v2
+        writeIORef' ref $ S c2 ii v2
 
 lookup :: Id -> Ids a -> IO (Maybe a)
 lookup (Id i) (Ids ref) = do
