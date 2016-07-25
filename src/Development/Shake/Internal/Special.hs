f5796d4dea6f245a59671b0cb0ba05f69dc75c90
Neil Mitchell <ndmitchell@gmail.com>
2016-07-25 21:07:33 +0100

    Oracle is no longer special for the lint check

--- src/Development/Shake/Internal/Special.hs
+++ src/Development/Shake/Internal/Special.hs
@@ -11,7 +11,7 @@ import Data.Typeable
 
 
 specialAlwaysRebuilds :: Value -> Bool
-specialAlwaysRebuilds v = con == "OracleA" || (con == "FileA" && show v == "File {mod=NEQ,size=NEQ,digest=NEQ}")
+specialAlwaysRebuilds v = con == "FileA" && show v == "File {mod=NEQ,size=NEQ,digest=NEQ}"
     where con = show $ fst $ splitTyConApp $ typeValue v
 
 
