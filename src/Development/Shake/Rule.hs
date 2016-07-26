560b176d450d01227b3399fd3d74fe765f7cb40b
Neil Mitchell <ndmitchell@gmail.com>
2016-07-26 16:57:16 +0100

    Don't export the LegacyRule pieces

--- src/Development/Shake/Rule.hs
+++ src/Development/Shake/Rule.hs
@@ -2,7 +2,7 @@
 -- | This module is used for defining new types of rules for Shake build systems.
 --   Most users will find the built-in set of rules sufficient.
 module Development.Shake.Rule(
-    LegacyRule(..), EqualCost(..), addLegacyRule, defaultLegacyRule, addUserRule, apply, apply1,
+    addUserRule, apply, apply1,
     addBuiltinRule, RunResult(..), RunChanged(..), BuiltinRun, BuiltinLint,
     trackUse, trackChange, trackAllow
     ) where
