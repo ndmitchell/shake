3a58ea09f49e238f7ca766b9c9617925e9af6332
Neil Mitchell <ndmitchell@gmail.com>
2016-07-26 09:49:40 +0100

    Remove the BS type entirely

--- src/Development/Shake/Internal/FileName.hs
+++ src/Development/Shake/Internal/FileName.hs
@@ -18,19 +18,6 @@ import Data.List
 -- Data.ByteString
 -- Mostly because ByteString does not have an NFData instance in GHC 7.4
 
--- | ASCII ByteString
-newtype BS = BS BS.ByteString
-    deriving (Hashable, Binary, Eq)
-
-instance Show BS where
-    show (BS x) = show x
-
-instance NFData BS where
-    -- some versions of ByteString do not have NFData instances, but seq is equivalent
-    -- for a strict bytestring. Therefore, we write our own instance.
-    rnf (BS x) = x `seq` ()
-
-
 -- | UTF8 ByteString
 newtype BSU = BSU BS.ByteString
     deriving (Hashable, Binary, Eq)
