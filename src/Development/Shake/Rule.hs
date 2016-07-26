786ee92110c00323b714b9f323359a408e8c151c
Neil Mitchell <ndmitchell@gmail.com>
2016-07-26 09:00:50 +0100

    Expose more things from Rule

--- src/Development/Shake/Rule.hs
+++ src/Development/Shake/Rule.hs
@@ -3,7 +3,7 @@
 --   Most users will find the built-in set of rules sufficient.
 module Development.Shake.Rule(
     LegacyRule(..), EqualCost(..), addLegacyRule, defaultLegacyRule, addUserRule, apply, apply1,
-    addBuiltinRule, RunResult(..), RunChanged(..),
+    addBuiltinRule, RunResult(..), RunChanged(..), BuiltinRun, BuiltinLint,
     trackUse, trackChange, trackAllow
     ) where
 
