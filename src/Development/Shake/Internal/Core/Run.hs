d7079aaf88b2da75c3ed8ef2ddf669bace3dbcae
Neil Mitchell <ndmitchell@gmail.com>
2016-08-11 22:04:31 +0100

    Reduce the apply function to only requiring Typable

--- src/Development/Shake/Internal/Core/Run.hs
+++ src/Development/Shake/Internal/Core/Run.hs
@@ -179,12 +179,12 @@ abbreviate abbrev = f
 -- | Execute a rule, returning the associated values. If possible, the rules will be run in parallel.
 --   This function requires that appropriate rules have been added with 'addUserRule'.
 --   All @key@ values passed to 'apply' become dependencies of the 'Action'.
-apply :: (ShakeValue key, ShakeValue value) => [key] -> Action [value]
+apply :: (ShakeValue key, Typeable value) => [key] -> Action [value]
 apply = applyForall
 
 -- We don't want the forall in the Haddock docs
 -- Don't short-circuit [] as we still want error messages
-applyForall :: forall key value . (ShakeValue key, ShakeValue value) => [key] -> Action [value]
+applyForall :: forall key value . (ShakeValue key, Typeable value) => [key] -> Action [value]
 applyForall ks = do
     -- this is the only place a user can inject a key into our world, so check they aren't throwing
     -- in unevaluated bottoms
