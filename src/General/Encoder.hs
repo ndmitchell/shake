f64581729543e75f2e9e65efca8a476890c892dc
Neil Mitchell <ndmitchell@gmail.com>
2016-08-12 10:04:10 +0100

    Switch to using a Builder instead of producing a ByteString in Encoder

--- src/General/Encoder.hs
+++ src/General/Encoder.hs
@@ -24,16 +24,24 @@ for2M_ as bs f = zipWithM_ f as bs
 ---------------------------------------------------------------------
 -- BINARY SERIALISATION
 
+data Builder = Builder {-# UNPACK #-} !Int (forall a . Ptr a -> Int -> IO ())
+
+instance Monoid Builder where
+    mempty = Builder 0 $ \_ _ -> return ()
+    mappend (Builder x1 x2) (Builder y1 y2) = Builder (x1+y1) $ \p i -> do x2 p i; y2 p $ i+x1
+
+
 -- | Methods for Binary serialisation that go directly between strict ByteString values.
 --   When the Database is read each key/value will be loaded as a separate ByteString,
 --   and for certain types (e.g. file rules) this may remain the preferred format for storing keys.
 --   Optimised for performance.
 class Encoder a where
-    encode :: a -> BS.ByteString
+    encode :: a -> Builder
     decode :: BS.ByteString -> a
 
 instance Encoder BS.ByteString where
-    encode = id
+    encode x = Builder n $ \ptr i -> BS.useAsCString x $ \bs -> BS.memcpy (castPtr bs) (ptr `plusPtr` i) (fromIntegral n)
+        where n = BS.length x
     decode = id
 
 instance Encoder [BS.ByteString] where
@@ -41,10 +49,10 @@ instance Encoder [BS.ByteString] where
     -- n :: Word32 - number of strings
     -- ns :: [Word32]{n} - length of each string
     -- contents of each string concatenated (sum ns bytes)
-    encode xs = BS.unsafeCreate (4 + (n * 4) + sum ns) $ \p -> do
-        pokeByteOff p 0 (fromIntegral n :: Word32)
-        for2M_ [4,8..] ns $ \i x -> pokeByteOff p i (fromIntegral x :: Word32)
-        p <- return $ p `plusPtr` (4 + (n * 4))
+    encode xs = Builder (4 + (n * 4) + sum ns) $ \p i -> do
+        pokeByteOff p i (fromIntegral n :: Word32)
+        for2M_ [4+i,8+i..] ns $ \i x -> pokeByteOff p i (fromIntegral x :: Word32)
+        p <- return $ p `plusPtr` (i + 4 + (n * 4))
         for2M_ (scanl (+) 0 ns) xs $ \i x -> BS.useAsCStringLen x $ \(bs, n) ->
             BS.memcpy (castPtr bs) (p `plusPtr` i) (fromIntegral n)
         where ns = map BS.length xs
@@ -56,16 +64,16 @@ instance Encoder [BS.ByteString] where
         return $ snd $ mapAccumL (\bs i -> swap $ BS.splitAt (fromIntegral i) bs) (BS.drop (4 + (n * 4)) bs) ns
 
 instance Encoder () where
-    encode () = BS.empty
+    encode () = mempty
     decode _ = ()
 
 instance Encoder String where
-    encode = UTF8.fromString
+    encode = encode . UTF8.fromString
     decode = UTF8.toString
 
 instance Encoder (Maybe String) where
-    encode Nothing = BS.empty
-    encode (Just xs) = UTF8.fromString $ '\0' : xs
+    encode Nothing = mempty
+    encode (Just xs) = encode $ UTF8.fromString $ '\0' : xs
     decode = fmap snd . uncons . UTF8.toString
 
 instance Encoder [String] where
@@ -77,12 +85,9 @@ instance Encoder (String, [String]) where
     decode x = let a:bs = decode x in (a,bs)
 
 instance Encoder Bool where
-    encode False = bsFalse
-    encode True = BS.empty
+    encode False = Builder 1 $ \ptr i -> pokeByteOff ptr i (0 :: Word8)
+    encode True = mempty
     decode = BS.null
-
--- CAF so the True ByteString is shared
-bsFalse = BS.singleton 0
 
 instance Encoder Word32 where
     encode = encodeStorable
@@ -93,9 +98,8 @@ instance Encoder Int where
     decode = decodeStorable
 
 
-encodeStorable :: forall a . Storable a => a -> BS.ByteString
-encodeStorable = \x -> BS.unsafeCreate n $ \p -> poke (castPtr p) x
-    where n = sizeOf (undefined :: a)
+encodeStorable :: forall a . Storable a => a -> Builder
+encodeStorable x = Builder (sizeOf x) $ \p i -> pokeByteOff p i x
 
 decodeStorable :: forall a . Storable a => BS.ByteString -> a
 decodeStorable = \bs -> unsafePerformIO $ BS.useAsCStringLen bs $ \(p, size) ->
@@ -103,9 +107,9 @@ decodeStorable = \bs -> unsafePerformIO $ BS.useAsCStringLen bs $ \(p, size) ->
     where n = sizeOf (undefined :: a)
 
 
-encodeStorableList :: forall a . Storable a => [a] -> BS.ByteString
-encodeStorableList = \xs -> BS.unsafeCreate (n * length xs) $ \p ->
-    for2M_ [0,n..] xs $ \i x -> pokeByteOff (castPtr p) i x
+encodeStorableList :: forall a . Storable a => [a] -> Builder
+encodeStorableList xs = Builder (n * length xs) $ \ptr i ->
+    for2M_ [i,i+n..] xs $ \i x -> pokeByteOff (castPtr ptr) i x
     where n = sizeOf (undefined :: a)
 
 decodeStorableList :: forall a . Storable a => BS.ByteString -> [a]
