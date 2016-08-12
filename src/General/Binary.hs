7a40b34b36b006d21e378cd2375d90f3e469f142
Neil Mitchell <ndmitchell@gmail.com>
2016-08-12 14:10:51 +0100

    Remove custom Binary instances that are no longer necessary

--- src/General/Binary.hs
+++ src/General/Binary.hs
@@ -4,8 +4,7 @@ module General.Binary(
     BinaryOp(..), newBinaryOp, encode', decode',
     binarySplit, binarySplit2, binarySplit3, unsafeBinarySplit, binaryCreate,
     Builder(..), runBuilder, sizeBuilder,
-    BinaryEx(..), putExStorable, getExStorable, putExStorableList, getExStorableList, putList, getList, putN, getN,
-    BinList(..), BinFloat(..)
+    BinaryEx(..), putExStorable, getExStorable, putExStorableList, getExStorableList, putList, getList, putN, getN
     ) where
 
 import Control.Monad
@@ -17,7 +16,6 @@ import Data.List.Extra
 import Data.Tuple.Extra
 import Foreign.Storable
 import Foreign.Ptr
-import Foreign.Marshal.Alloc
 import System.IO.Unsafe as U
 import qualified Data.ByteString as BS
 import qualified Data.ByteString.Internal as BS
@@ -232,38 +230,3 @@ getN bs
     = (BS.unsafeTake n bs, BS.unsafeDrop n bs)
     | otherwise = error "getList, corrupted binary"
     where len = BS.length bs
-
-
----------------------------------------------------------------------
--- BINARY
-
-newtype BinList a = BinList {fromBinList :: [a]}
-
-instance Show a => Show (BinList a) where show = show . fromBinList
-
-instance Binary a => Binary (BinList a) where
-    put (BinList xs) = case splitAt 254 xs of
-        (_, []) -> putWord8 (genericLength xs) >> mapM_ put xs
-        (a, b) -> putWord8 255 >> mapM_ put a >> put (BinList b)
-    get = do
-        x <- getWord8
-        case x of
-            255 -> do xs <- replicateM 254 get; BinList ys <- get; return $ BinList $ xs ++ ys
-            n -> BinList <$> replicateM (fromInteger $ toInteger n) get
-
-
-newtype BinFloat = BinFloat {fromBinFloat :: Float}
-
-instance Show BinFloat where show = show . fromBinFloat
-
-instance Binary BinFloat where
-    put (BinFloat x) = put (convert x :: Word32)
-    get = fmap (BinFloat . convert) (get :: Get Word32)
-
-
--- Originally from data-binary-ieee754 package
-
-convert :: (Storable a, Storable b) => a -> b
-convert x = U.unsafePerformIO $ alloca $ \buf -> do
-    poke (castPtr buf) x
-    peek buf
