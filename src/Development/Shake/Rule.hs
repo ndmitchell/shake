98ffe628a82d459fe7def07f82f524a70d020864
Neil Mitchell <ndmitchell@gmail.com>
2016-08-22 19:03:45 +0100

    Tweak the module doc headers

--- src/Development/Shake/Rule.hs
+++ src/Development/Shake/Rule.hs
@@ -2,10 +2,9 @@
 -- | This module is used for defining new types of rules for Shake build systems.
 --   Most users will find the built-in set of rules sufficient.
 module Development.Shake.Rule(
-    -- * Builtin rules
-    -- ** Defining
+    -- * Defining builtin rules
     BuiltinLint, noLint, BuiltinRun, RunChanged(..), RunResult(..), addBuiltinRule,
-    -- ** Running
+    -- * Calling builtin rules
     apply, apply1,
     -- * User rules
     UserRule(..), addUserRule, getUserRules, userRuleMatch,
