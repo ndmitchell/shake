8defed5f34d0ff425b00c1f67d53d2cf4800b854
Neil Mitchell <ndmitchell@gmail.com>
2016-09-28 07:37:32 +0100

    Change the manual test to put everything under .git, where we don't inadvertantly index it

--- src/Test/Manual.hs
+++ src/Test/Manual.hs
@@ -11,13 +11,16 @@ main = shaken test $ \args obj ->
     action $ liftIO $ error "The 'manual' example should only be used in test mode"
 
 test build obj = do
-    copyDirectoryChanged "docs/manual" $ obj "manual"
-    copyDirectoryChanged "src/Development" $ obj "manual/Development"
-    copyDirectoryChanged "src/General" $ obj "manual/General"
-    copyFileChanged "src/Paths.hs" $ obj "manual/Paths_shake.hs"
+    -- we use .git as our destination, despite not being a real git repo
+    -- so that search tools ignore it, and I don't get dupes for every source file
+    let dest = obj ".git"
+    copyDirectoryChanged "docs/manual" $ dest
+    copyDirectoryChanged "src/Development" $ dest </> "Development"
+    copyDirectoryChanged "src/General" $ dest </> "General"
+    copyFileChanged "src/Paths.hs" $ dest </> "Paths_shake.hs"
     let cmdline = if isWindows then "build.bat" else "/bin/sh build.sh"
-    () <- cmd [Cwd $ obj "manual", Shell] cmdline "-j2"
-    assertExists $ obj "manual/_build/run" <.> exe
-    () <- cmd [Cwd $ obj "manual", Shell] cmdline
-    () <- cmd [Cwd $ obj "manual", Shell] [cmdline,"clean"]
-    assertMissing $ obj "manual/_build/run" <.> exe
+    () <- cmd [Cwd dest, Shell] cmdline "-j2"
+    assertExists $ dest </> "_build/run" <.> exe
+    () <- cmd [Cwd dest, Shell] cmdline
+    () <- cmd [Cwd dest, Shell] [cmdline,"clean"]
+    assertMissing $ dest </> "_build/run" <.> exe
