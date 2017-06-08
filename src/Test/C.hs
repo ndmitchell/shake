a32164560490f58bbc95a7739bfc4bbad012d829
Neil Mitchell <ndmitchell@gmail.com>
2017-06-08 08:31:52 +0100

    Convert C to shakeTest

--- src/Test/C.hs
+++ src/Test/C.hs
@@ -5,8 +5,9 @@ import Development.Shake
 import Development.Shake.FilePath
 import Test.Type
 
-main = shaken noTest $ \args obj -> do
-    let src = "src/Test/C"
+main = shakeTest_ noTest2 $ do
+    let obj = id
+    let src = root </> "src/Test/C"
     want [obj "Main.exe"]
 
     obj "Main.exe" %> \out -> do
