f13f4a32ed4b668cbf99b2db0f091918b976c947
Neil Mitchell <ndmitchell@gmail.com>
2017-06-08 08:31:52 +0100

    Convert Manual to shakeTest

--- src/Test/Manual.hs
+++ src/Test/Manual.hs
@@ -7,17 +7,17 @@ import Test.Type
 import System.Info.Extra
 
 
-main = shaken test $ \args obj ->
-    action $ liftIO $ error "The 'manual' example should only be used in test mode"
+main = shakeTest_ test $
+    action $ fail "The 'manual' example should only be used in test mode"
 
-test build obj = do
+test build = do
     -- we use .git as our destination, despite not being a real git repo
     -- so that search tools ignore it, and I don't get dupes for every source file
-    let dest = obj ".git"
-    copyDirectoryChanged "docs/manual" dest
-    copyDirectoryChanged "src/Development" $ dest </> "Development"
-    copyDirectoryChanged "src/General" $ dest </> "General"
-    copyFileChanged "src/Paths.hs" $ dest </> "Paths_shake.hs"
+    let dest = ".git"
+    copyDirectoryChanged (root </> "docs/manual") dest
+    copyDirectoryChanged (root </> "src/Development") $ dest </> "Development"
+    copyDirectoryChanged (root </> "src/General") $ dest </> "General"
+    copyFileChanged (root </> "src/Paths.hs") $ dest </> "Paths_shake.hs"
     let cmdline = if isWindows then "build.bat" else "/bin/sh build.sh"
     () <- cmd [Cwd dest, Shell] cmdline "-j2"
     assertExists $ dest </> "_build/run" <.> exe
