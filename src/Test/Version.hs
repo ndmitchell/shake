889ef6c108c0f8b2a246d22a1bb2d81901b84287
Neil Mitchell <ndmitchell@gmail.com>
2017-06-11 20:50:27 +0200

    Delete obj = id from Version

--- src/Test/Version.hs
+++ src/Test/Version.hs
@@ -6,30 +6,28 @@ import Test.Type
 
 
 main = shakeTest_ test $ do
-    let obj = id
-    want [obj "foo.txt"]
-    obj "foo.txt" %> \file -> liftIO $ appendFile file "x"
+    want ["foo.txt"]
+    "foo.txt" %> \file -> liftIO $ appendFile file "x"
 
 test build = do
-    let obj = id
-    writeFile (obj "foo.txt") ""
-    v1 <- getHashedShakeVersion [obj "foo.txt"]
-    writeFile (obj "foo.txt") "y"
-    v2 <- getHashedShakeVersion [obj "foo.txt"]
+    writeFile "foo.txt" ""
+    v1 <- getHashedShakeVersion ["foo.txt"]
+    writeFile "foo.txt" "y"
+    v2 <- getHashedShakeVersion ["foo.txt"]
     assertBool (v1 /= v2) "Hashes must not be equal"
 
     build ["clean"]
     build []
-    assertContents (obj "foo.txt") "x"
+    assertContents "foo.txt" "x"
     build ["--rule-version=new","--silent"]
-    assertContents (obj "foo.txt") "xx"
+    assertContents "foo.txt" "xx"
     build ["--rule-version=new"]
-    assertContents (obj "foo.txt") "xx"
+    assertContents "foo.txt" "xx"
     build ["--rule-version=extra","--silent"]
-    assertContents (obj "foo.txt") "xxx"
+    assertContents "foo.txt" "xxx"
     build ["--rule-version=more","--no-rule-version"]
-    assertContents (obj "foo.txt") "xxx"
+    assertContents "foo.txt" "xxx"
     build ["--rule-version=more"]
-    assertContents (obj "foo.txt") "xxx"
+    assertContents "foo.txt" "xxx"
     build ["--rule-version=final","--silent"]
-    assertContents (obj "foo.txt") "xxxx"
+    assertContents "foo.txt" "xxxx"
