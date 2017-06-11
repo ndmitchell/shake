df8d6fe778149024210532af074e0ebddc4ca81b
Neil Mitchell <ndmitchell@gmail.com>
2017-06-11 16:19:53 +0200

    Delete obj = id from live

--- src/Test/Live.hs
+++ src/Test/Live.hs
@@ -6,24 +6,21 @@ import Test.Type
 
 
 main = shakeTest_ test $ do
-    let obj = id
-
-    obj "foo" %> \ out -> do
-        need [obj "bar"]
+    "foo" %> \ out -> do
+        need ["bar"]
         writeFile' out ""
 
-    obj "bar" %> \out -> writeFile' out ""
-    obj "baz" %> \out -> writeFile' out ""
+    "bar" %> \out -> writeFile' out ""
+    "baz" %> \out -> writeFile' out ""
 
 
 test build = do
-    let obj = id
     build ["clean"]
-    build ["foo","baz","--live=" ++ obj "live.txt"]
-    assertContentsUnordered (obj "live.txt") $ map obj $ words "foo bar baz"
-    build ["foo","baz","--live=" ++ obj "live.txt"]
-    assertContentsUnordered (obj "live.txt") $ map obj $ words "foo bar baz"
-    build ["foo","--live=" ++ obj "live.txt"]
-    assertContentsUnordered (obj "live.txt") $ map obj $ words "foo bar"
-    build ["bar","--live=" ++ obj "live.txt"]
-    assertContentsUnordered (obj "live.txt") $ map obj $ words "bar"
+    build ["foo","baz","--live=live.txt"]
+    assertContentsUnordered "live.txt" $ words "foo bar baz"
+    build ["foo","baz","--live=live.txt"]
+    assertContentsUnordered "live.txt" $ words "foo bar baz"
+    build ["foo","--live=live.txt"]
+    assertContentsUnordered "live.txt" $ words "foo bar"
+    build ["bar","--live=live.txt"]
+    assertContentsUnordered "live.txt" $ words "bar"
