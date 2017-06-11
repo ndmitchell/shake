98c69dce7c1d919525a44f46017221bcbeca6148
Neil Mitchell <ndmitchell@gmail.com>
2017-06-11 20:40:31 +0200

    Remove obj = id from Files

--- src/Test/Files.hs
+++ src/Test/Files.hs
@@ -11,20 +11,19 @@ import General.GetOpt
 data Args = UsePredicate deriving (Eq,Show,Bounded,Enum)
 
 main = shakeTest test optionsEnum $ \opts -> do
-    let obj = id
     want ["even.txt","odd.txt"]
 
     -- Since &?> and &%> are implemented separately we test everything in both modes
     let deps &?%> act | UsePredicate `elem` opts = (\x -> if x `elem` deps then Just deps else Nothing) &?> act
                       | otherwise = deps &%> act
 
-    map obj ["even.txt","odd.txt"] &?%> \[evens,odds] -> do
-        src <- readFileLines $ obj "numbers.txt"
+    ["even.txt","odd.txt"] &?%> \[evens,odds] -> do
+        src <- readFileLines "numbers.txt"
         let (es,os) = partition even $ map read src
         writeFileLines evens $ map show es
         writeFileLines odds  $ map show os
 
-    map obj ["dir1/out.txt","dir2/out.txt"] &?%> \[a,b] -> do
+    ["dir1/out.txt","dir2/out.txt"] &?%> \[a,b] -> do
         writeFile' a "a"
         writeFile' b "b"
 
@@ -34,13 +33,12 @@ main = shakeTest test optionsEnum $ \opts -> do
 
 
 test build = do
-    let obj = id
     forM_ [[],["--usepredicate"]] $ \args -> do
         let nums = unlines . map show
-        writeFile (obj "numbers.txt") $ nums [1,2,4,5,2,3,1]
+        writeFile "numbers.txt" $ nums [1,2,4,5,2,3,1]
         build ("--sleep":args)
-        assertContents (obj "even.txt") $ nums [2,4,2]
-        assertContents (obj "odd.txt" ) $ nums [1,5,3,1]
+        assertContents "even.txt" $ nums [2,4,2]
+        assertContents "odd.txt"  $ nums [1,5,3,1]
         build ["clean"]
         build ["--no-build","--report=-"]
         build ["dir1/out.txt"]
