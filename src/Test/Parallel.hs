4a1266b5bcb29894eb29a0a9c87a5cf893a56074
Neil Mitchell <ndmitchell@gmail.com>
2017-06-11 20:40:55 +0200

    Remove Parallel from obj = id

--- src/Test/Parallel.hs
+++ src/Test/Parallel.hs
@@ -10,21 +10,19 @@ import Data.IORef
 
 
 main = shakeTest_ test $ do
-    let obj = id
-
-    obj "AB.txt" %> \out -> do
+    "AB.txt" %> \out -> do
         -- need [obj "A.txt", obj "B.txt"]
-        (text1,text2) <- readFile' (obj "A.txt") `par` readFile' (obj "B.txt")
+        (text1,text2) <- readFile' "A.txt" `par` readFile' "B.txt"
         writeFile' out $ text1 ++ text2
 
     phony "cancel" $ do
-        writeFile' (obj "cancel") ""
+        writeFile' "cancel" ""
         done <- liftIO $ newIORef 0
         lock <- liftIO newLock
         void $ parallel $ replicate 5 $ liftIO $ do
             x <- atomicModifyIORef done $ dupe . succ
             when (x == 3) $ do sleep 0.1; fail "boom"
-            withLock lock $ appendFile (obj "cancel") "x"
+            withLock lock $ appendFile "cancel" "x"
 
     phony "parallel" $ do
         active <- liftIO $ newIORef 0
@@ -35,22 +33,21 @@ main = shakeTest_ test $ do
             sleep 0.1
             atomicModifyIORef active $ dupe . pred
         peak <- liftIO $ readIORef peak
-        writeFile' (obj "parallel") $ show peak
+        writeFile' "parallel" $ show peak
 
 
 test build = do
-    let obj = id
-    writeFile (obj "A.txt") "AAA"
-    writeFile (obj "B.txt") "BBB"
+    writeFile "A.txt" "AAA"
+    writeFile "B.txt" "BBB"
     build ["AB.txt","--sleep"]
-    assertContents (obj "AB.txt") "AAABBB"
-    appendFile (obj "A.txt") "aaa"
+    assertContents "AB.txt" "AAABBB"
+    appendFile "A.txt" "aaa"
     build ["AB.txt"]
-    assertContents (obj "AB.txt") "AAAaaaBBB"
+    assertContents "AB.txt" "AAAaaaBBB"
 
     assertException ["boom"] $ build ["cancel","-j1","--quiet"]
-    assertContents (obj "cancel") "xx"
+    assertContents "cancel" "xx"
     build ["parallel","-j1"]
-    assertContents (obj "parallel") "1"
+    assertContents "parallel" "1"
     build ["parallel","-j5"]
-    assertContents (obj "parallel") "5"
+    assertContents "parallel" "5"
