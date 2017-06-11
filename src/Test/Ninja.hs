e2d9a5d14f34a132b666e3c0e1bd11f151ee54a7
Neil Mitchell <ndmitchell@gmail.com>
2017-06-11 16:26:21 +0200

    Delete obj = id in Ninja

--- src/Test/Ninja.hs
+++ src/Test/Ninja.hs
@@ -27,50 +27,49 @@ main = shakeTest test [opts] $ \opts -> do
 
 
 test build = do
-    let obj = id
     let runEx ninja shake = build $ "--exception" : map ("--arg=" ++) (words ninja) ++ words shake
     let run ninja = runEx ninja []
     let runFail ninja bad = assertException [bad] $ runEx ninja "--quiet"
 
     build ["clean"]
     run "-f../../src/Test/Ninja/test1.ninja"
-    assertExists $ obj "out1.txt"
+    assertExists "out1.txt"
 
     run "-f../../src/Test/Ninja/test2.ninja"
-    assertExists $ obj "out2.2"
-    assertMissing $ obj "out2.1"
+    assertExists "out2.2"
+    assertMissing "out2.1"
     build ["clean"]
     run "-f../../src/Test/Ninja/test2.ninja out2.1"
-    assertExists $ obj "out2.1"
-    assertMissing $ obj "out2.2"
+    assertExists "out2.1"
+    assertMissing "out2.2"
 
-    copyFile "../../src/Test/Ninja/test3-sub.ninja" $ obj "test3-sub.ninja"
-    copyFile "../../src/Test/Ninja/test3-inc.ninja" $ obj "test3-inc.ninja"
-    createDirectoryIfMissing True $ obj "subdir"
-    copyFile "../../src/Test/Ninja/subdir/1.ninja" $ obj "subdir/1.ninja"
-    copyFile "../../src/Test/Ninja/subdir/2.ninja" $ obj "subdir/2.ninja"
+    copyFile "../../src/Test/Ninja/test3-sub.ninja" "test3-sub.ninja"
+    copyFile "../../src/Test/Ninja/test3-inc.ninja" "test3-inc.ninja"
+    createDirectoryIfMissing True "subdir"
+    copyFile "../../src/Test/Ninja/subdir/1.ninja" "subdir/1.ninja"
+    copyFile "../../src/Test/Ninja/subdir/2.ninja" "subdir/2.ninja"
     run "-f../../src/Test/Ninja/test3.ninja"
-    assertContentsWords (obj "out3.1") "g4+b1+++i1"
-    assertContentsWords (obj "out3.2") "g4++++i1"
-    assertContentsWords (obj "out3.3") "g4++++i1"
-    assertContentsWords (obj "out3.4") "g4+++s1+s2"
+    assertContentsWords "out3.1" "g4+b1+++i1"
+    assertContentsWords "out3.2" "g4++++i1"
+    assertContentsWords "out3.3" "g4++++i1"
+    assertContentsWords "out3.4" "g4+++s1+s2"
 
     run "-f../../src/Test/Ninja/test4.ninja out"
-    assertExists $ obj "out.txt"
-    assertExists $ obj "out2.txt"
+    assertExists "out.txt"
+    assertExists "out2.txt"
 
     run "-f../../src/Test/Ninja/test5.ninja"
-    assertExists $ obj "output file"
+    assertExists "output file"
 
-    writeFile (obj "nocreate.log") ""
-    writeFile (obj "nocreate.in") ""
+    writeFile "nocreate.log" ""
+    writeFile "nocreate.in" ""
     run "-f../../src/Test/Ninja/nocreate.ninja"
-    assertContentsWords (obj "nocreate.log") "x"
+    assertContentsWords "nocreate.log" "x"
     run "-f../../src/Test/Ninja/nocreate.ninja"
     run "-f../../src/Test/Ninja/nocreate.ninja"
-    assertContentsWords (obj "nocreate.log") "x x x"
+    assertContentsWords "nocreate.log" "x x x"
 
-    writeFile (obj "input") ""
+    writeFile "input" ""
     runFail "-f../../src/Test/Ninja/lint.ninja bad --lint" "'needed' file required rebuilding"
     run "-f../../src/Test/Ninja/lint.ninja good --lint"
     runFail "-f../../src/Test/Ninja/lint.ninja bad --lint" "not a pre-dependency"
@@ -83,7 +82,7 @@ test build = do
     zipWithM_ eq want res
 
     -- Test initial variable bindings and variables in include/subninja statements
-    let test6 = obj "test6"
+    let test6 = "test6"
 
     copyFile "../../src/Test/Ninja/test6-sub.ninja" $ test6 ++ "-sub.ninja"
     copyFile "../../src/Test/Ninja/test6-inc.ninja" $ test6 ++ "-inc.ninja"
@@ -97,21 +96,21 @@ test build = do
 
     -- tests from ninjasmith: https://github.com/ndmitchell/ninjasmith/
     run "-f../../src/Test/Ninja/redefine.ninja"
-    assertContentsWords (obj "redefine.txt") "version3 version2"
+    assertContentsWords "redefine.txt" "version3 version2"
 
     run "-f../../src/Test/Ninja/buildseparate.ninja"
-    assertContentsWords (obj "buildseparate.txt") "XX"
+    assertContentsWords "buildseparate.txt" "XX"
 
     run "-f../../src/Test/Ninja/lexical.ninja"
-    assertContentsWords (obj "lexical.txt") "XFoo_BarXXFooX.bar"
+    assertContentsWords "lexical.txt" "XFoo_BarXXFooX.bar"
 
     when False $ do
         -- currently fails because Shake doesn't match Ninja here
         run "-f../../src/Test/Ninja/outputtouch.ninja"
-        assertContentsWords (obj "outputtouch.txt") "hello"
-        writeFile (obj "outputtouch.txt") "goodbye"
+        assertContentsWords "outputtouch.txt" "hello"
+        writeFile "outputtouch.txt" "goodbye"
         run "-f../../src/Test/Ninja/outputtouch.ninja"
-        assertContentsWords (obj "outputtouch.txt") "goodbye"
-        removeFile (obj "outputtouch.txt")
+        assertContentsWords "outputtouch.txt" "goodbye"
+        removeFile "outputtouch.txt"
         run "-f../../src/Test/Ninja/outputtouch.ninja"
-        assertContentsWords (obj "outputtouch.txt") "hello"
+        assertContentsWords "outputtouch.txt" "hello"
