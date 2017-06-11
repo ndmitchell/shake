abc908b24590f5ef3fe39c9fd2f7349786f36a20
Neil Mitchell <ndmitchell@gmail.com>
2017-06-11 16:14:31 +0200

    Delete obj = id in digest

--- src/Test/Digest.hs
+++ src/Test/Digest.hs
@@ -7,37 +7,35 @@ import Test.Type
 
 
 main = shakeTest_ test $ do
-    let obj = id
-    want [obj "Out.txt",obj "Out2.txt"]
+    want ["Out.txt","Out2.txt"]
 
-    obj "Out.txt" %> \out -> do
-        txt <- readFile' $ obj "In.txt"
+    "Out.txt" %> \out -> do
+        txt <- readFile' "In.txt"
         liftIO $ appendFile out txt
 
-    [obj "Out1.txt",obj "Out2.txt"] &%> \[out1,out2] -> do
-        txt <- readFile' $ obj "In.txt"
+    ["Out1.txt","Out2.txt"] &%> \[out1,out2] -> do
+        txt <- readFile' "In.txt"
         liftIO $ appendFile out1 txt
         liftIO $ appendFile out2 txt
 
-    [obj "Bug1.txt",obj "Bug2.txt"] &%> \[out1,out2] -> do
-        need [obj "Bug3.txt"]
+    ["Bug1.txt","Bug2.txt"] &%> \[out1,out2] -> do
+        need ["Bug3.txt"]
         writeFile' out1 "X"
         writeFile' out2 "Y"
 
     "leaf" ~> return ()
-    obj "node1.txt" %> \file -> do need ["leaf"]; writeFile' file "x"
-    obj "node2.txt" %> \file -> do need [obj "node1.txt"]; liftIO $ appendFile file "x"
+    "node1.txt" %> \file -> do need ["leaf"]; writeFile' file "x"
+    "node2.txt" %> \file -> do need ["node1.txt"]; liftIO $ appendFile file "x"
 
-    [obj "rewrite1",obj "rewrite2"] &%> \outs -> do
+    ["rewrite1","rewrite2"] &%> \outs -> do
         alwaysRerun
         forM_ outs $ \out -> writeFile' out "rewrite"
 
 
 test build = do
-    let obj = id
-    let outs = take 1 $ map obj ["Out.txt","Out1.txt","Out2.txt"]
+    let outs = ["Out.txt","Out1.txt","Out2.txt"]
     let writeOut x = forM_ outs $ \out -> writeFile out x
-    let writeIn = writeFile (obj "In.txt")
+    let writeIn = writeFile "In.txt"
     let assertOut x = forM_ outs $ \out -> assertContents out x
 
     writeOut ""
@@ -94,14 +92,14 @@ test build = do
 
     -- test for #218
     forM_ [("--digest",1),("--digest-and",1),("--digest-or",2),("--digest-and-input",2),("",2)] $ \(flag,count) -> do
-        writeFile (obj "node2.txt") "y"
+        writeFile "node2.txt" "y"
         replicateM_ 2 $ build $ ["node2.txt","--sleep"] ++ [flag | flag /= ""]
-        assertContents (obj "node2.txt") $ 'y' : replicate count 'x'
+        assertContents "node2.txt" $ 'y' : replicate count 'x'
 
     -- test for #296
-    writeFile (obj "Bug3.txt") "X"
+    writeFile "Bug3.txt" "X"
     build ["--digest-and-input","Bug1.txt","--sleep"]
-    writeFile (obj "Bug3.txt") "Y"
+    writeFile "Bug3.txt" "Y"
     build ["--digest-and-input","Bug1.txt","--lint"]
 
     -- test for #427
