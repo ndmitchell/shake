c95fbf95e7ec5858316046d2d7babe92f4bef401
Neil Mitchell <ndmitchell@gmail.com>
2017-06-11 15:57:03 +0200

    Delete obj = id in cache

--- src/Test/Cache.hs
+++ src/Test/Cache.hs
@@ -8,33 +8,31 @@ import Test.Type
 
 
 main = shakeTest_ test $ do
-    let obj = id
     vowels <- newCache $ \file -> do
         src <- readFile' file
-        liftIO $ appendFile (obj "trace.txt") "1"
+        liftIO $ appendFile "trace.txt" "1"
         return $ length $ filter isDigit src
-    obj "*.out*" %> \x ->
+    "*.out*" %> \x ->
         writeFile' x . show =<< vowels (dropExtension x <.> "txt")
 
 
 test build = do
-    let obj = id
-    writeFile (obj "trace.txt") ""
-    writeFile (obj "vowels.txt") "abc123a"
+    writeFile "trace.txt" ""
+    writeFile "vowels.txt" "abc123a"
     build ["vowels.out1","vowels.out2","-j3","--sleep"]
-    assertContents (obj "trace.txt") "1"
-    assertContents (obj "vowels.out1") "3"
-    assertContents (obj "vowels.out2") "3"
+    assertContents "trace.txt" "1"
+    assertContents "vowels.out1" "3"
+    assertContents "vowels.out2" "3"
 
     build ["vowels.out2","-j3"]
-    assertContents (obj "trace.txt") "1"
-    assertContents (obj "vowels.out1") "3"
+    assertContents "trace.txt" "1"
+    assertContents "vowels.out1" "3"
 
-    writeFile (obj "vowels.txt") "12xyz34"
+    writeFile "vowels.txt" "12xyz34"
     build ["vowels.out2","-j3","--sleep"]
-    assertContents (obj "trace.txt") "11"
-    assertContents (obj "vowels.out2") "4"
+    assertContents "trace.txt" "11"
+    assertContents "vowels.out2" "4"
 
     build ["vowels.out1","-j3","--sleep"]
-    assertContents (obj "trace.txt") "111"
-    assertContents (obj "vowels.out1") "4"
+    assertContents "trace.txt" "111"
+    assertContents "vowels.out1" "4"
