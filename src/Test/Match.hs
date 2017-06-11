ac5ec7c76fa535fae7caa0f9902668fec6ffa752
Neil Mitchell <ndmitchell@gmail.com>
2017-06-11 16:30:54 +0200

    Delete obj = id from Match

--- src/Test/Match.hs
+++ src/Test/Match.hs
@@ -7,58 +7,56 @@ import Test.Type
 
 
 main = shakeTest_ test $ do
-    let obj = id
     let output x file = writeFile' file x
 
     ["or*","*or"] |%> output ""
 
     alternatives $ do
-        obj "alternative.t*" %> output "alternative.t*"
-        obj "alternative.*" %> output "alternative.*"
+        "alternative.t*" %> output "alternative.t*"
+        "alternative.*" %> output "alternative.*"
 
-    priority 100 $ priority 0 $ obj "priority.txt" %> output "100"
-    priority 50 $ obj "priority.txt" %> output "50"
-
-    alternatives $ do
-        priority 20 $ obj "altpri.txt" %> output "20"
-        priority 40 $ obj "altpri.txt" %> output "40"
-    priority 30 $ obj "altpri.txt" %> output "30"
+    priority 100 $ priority 0 $ "priority.txt" %> output "100"
+    priority 50 $ "priority.txt" %> output "50"
 
     alternatives $ do
-        priority 21 $ obj "altpri2.txt" %> output "21"
-        priority 22 $ obj "altpri2.txt" %> output "22"
-    priority 23 $ obj "altpri2.txt" %> output "23"
+        priority 20 $ "altpri.txt" %> output "20"
+        priority 40 $ "altpri.txt" %> output "40"
+    priority 30 $ "altpri.txt" %> output "30"
 
-    priority 55 $ alternatives $ obj "x" %> output "55"
-    priority 51 $ obj "x" %> output "51"
+    alternatives $ do
+        priority 21 $ "altpri2.txt" %> output "21"
+        priority 22 $ "altpri2.txt" %> output "22"
+    priority 23 $ "altpri2.txt" %> output "23"
 
-    priority 42 $ alternatives $ obj "xx" %> output "42"
-    priority 43 $ obj "xx" %> output "43"
+    priority 55 $ alternatives $ "x" %> output "55"
+    priority 51 $ "x" %> output "51"
+
+    priority 42 $ alternatives $ "xx" %> output "42"
+    priority 43 $ "xx" %> output "43"
 
     priority 10 $ do
-        priority 7 $ obj "change" %> output "7"
-        priority 8 $ obj "change" %> output "8"
-    priority 9 $ obj "change" %> output "9"
+        priority 7 $ "change" %> output "7"
+        priority 8 $ "change" %> output "8"
+    priority 9 $ "change" %> output "9"
 
 
 test build = do
-    let obj = id
     build ["clean"]
     build ["or"]
 
     build ["alternative.foo","alternative.txt"]
-    assertContents (obj "alternative.foo") "alternative.*"
-    assertContents (obj "alternative.txt") "alternative.t*"
+    assertContents "alternative.foo" "alternative.*"
+    assertContents "alternative.txt" "alternative.t*"
 
     build ["priority.txt"]
-    assertContents (obj "priority.txt") "100"
+    assertContents "priority.txt" "100"
 
     build ["altpri.txt","altpri2.txt"]
-    assertContents (obj "altpri.txt") "20"
-    assertContents (obj "altpri2.txt") "23"
+    assertContents "altpri.txt" "20"
+    assertContents "altpri2.txt" "23"
 
     build ["x","xx"]
-    assertContents (obj "x") "55"
-    assertContents (obj "xx") "43"
+    assertContents "x" "55"
+    assertContents "xx" "43"
 
     assertException ["matches multiple rules"] $ build ["change","--quiet"]
