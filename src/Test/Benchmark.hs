b036cb5f7edd5852aff18e40b8ff4c507fb40923
Neil Mitchell <ndmitchell@gmail.com>
2017-06-11 15:52:09 +0200

    Delete obj from benchmark

--- src/Test/Benchmark.hs
+++ src/Test/Benchmark.hs
@@ -15,14 +15,13 @@ readEither = Right . read -- FIXME: Use the version from extra once available
 
 -- | Given a breadth and depth come up with a set of build files
 main = shakeTest test opts $ \opts -> do
-    let obj = id
     let depth   = last $ error "Missing --depth"   : [x | Depth   x <- opts]
     let breadth = last $ error "Missing --breadth" : [x | Breadth x <- opts]
 
-    want [obj $ "0." ++ show i | i <- [1..breadth]]
-    obj "*" %> \out -> do
+    want ["0." ++ show i | i <- [1..breadth]]
+    "*" %> \out -> do
         let d = read $ takeBaseName out
-        need [obj $ show (d + 1) ++ "." ++ show i | d < depth, i <- [1..breadth]]
+        need [show (d + 1) ++ "." ++ show i | d < depth, i <- [1..breadth]]
         writeFile' out ""
 
 test build = do
