963c7b2e5dba93375b2495fe5d0c2c0d0c432223
Neil Mitchell <ndmitchell@gmail.com>
2017-06-11 20:40:10 +0200

    Remove obj = id from Basic

--- src/Test/Basic.hs
+++ src/Test/Basic.hs
@@ -14,21 +14,19 @@ import Prelude
 
 
 main = shakeTest_ test $ do
-    let obj = id
-
-    obj "AB.txt" %> \out -> do
-        need [obj "A.txt", obj "B.txt"]
-        text1 <- readFile' $ obj "A.txt"
-        text2 <- readFile' $ obj "B.txt"
+    "AB.txt" %> \out -> do
+        need ["A.txt", "B.txt"]
+        text1 <- readFile' "A.txt"
+        text2 <- readFile' "B.txt"
         writeFile' out $ text1 ++ text2
 
-    obj "twice.txt" %> \out -> do
-        let src = obj "once.txt"
+    "twice.txt" %> \out -> do
+        let src = "once.txt"
         need [src, src]
         copyFile' src out
 
-    obj "once.txt" %> \out -> do
-        src <- readFile' $ obj "zero.txt"
+    "once.txt" %> \out -> do
+        src <- readFile' "zero.txt"
         writeFile' out src
 
     phonys $ \x -> if x /= "halfclean" then Nothing else Just $
@@ -37,86 +35,85 @@ main = shakeTest_ test $ do
     phony "cleaner" $
         removeFilesAfter "dir" ["//*"]
 
-    phony (obj "configure") $
-        liftIO $ appendFile (obj "configure") "1"
+    phony "configure" $
+        liftIO $ appendFile "configure" "1"
 
     phony "install" $ do
-        need [obj "configure",obj "once.txt"]
-        liftIO $ appendFile (obj "install") "1"
+        need ["configure","once.txt"]
+        liftIO $ appendFile "install" "1"
 
     phony "duplicate1" $ need ["duplicate2","duplicate3"]
     phony "duplicate2" $ need ["duplicate3"]
-    phony "duplicate3" $ liftIO $ appendFile (obj "duplicate") "1"
+    phony "duplicate3" $ liftIO $ appendFile "duplicate" "1"
 
     phony "dummy" $
-        liftIO $ appendFile (obj "dummy") "1"
+        liftIO $ appendFile "dummy" "1"
 
     phony "threads" $ do
         x <- getShakeOptions
-        writeFile' (obj "threads.txt") $ show $ shakeThreads x
+        writeFile' "threads.txt" $ show $ shakeThreads x
 
     phony ("slash" </> "platform") $ return ()
     phony "slash/forward" $ return ()
 
-    obj "dummer.txt" %> \out -> do
+    "dummer.txt" %> \out -> do
         need ["dummy","dummy"]
         need ["dummy"]
         liftIO $ appendFile out "1"
 
     r <- newResource ".log file" 1
-    let trace x = withResource r 1 $ liftIO $ appendFile (obj ".log") x
-    obj "*.par" %> \out -> do
+    let trace x = withResource r 1 $ liftIO $ appendFile ".log" x
+    "*.par" %> \out -> do
         trace "["
         (if "unsafe" `isInfixOf` out then unsafeExtraThread else id) $ liftIO $ sleep 0.1
         trace "]"
         writeFile' out out
 
-    obj "sep" </> "1.txt" %> \out -> writeFile' out ""
-    obj "sep/2.txt" %> \out -> writeFile' out ""
-    [obj "sep" </> "3.txt", obj "sep" </> "4.txt", obj "sep" </> "5.*", obj "sep/6.txt"] |%> \out -> writeFile' out ""
-    [obj "sep" </> "7.txt"] |%> \out -> writeFile' out ""
+    "sep" </> "1.txt" %> \out -> writeFile' out ""
+    "sep/2.txt" %> \out -> writeFile' out ""
+    ["sep" </> "3.txt", "sep" </> "4.txt", "sep" </> "5.*", "sep/6.txt"] |%> \out -> writeFile' out ""
+    ["sep" </> "7.txt"] |%> \out -> writeFile' out ""
 
-    obj "ids/source" %> \out -> return ()
-    obj "ids/out" %> \out -> do need =<< readFileLines (obj "ids/source"); writeFile' out ""
-    obj "ids/*" %> \out -> do alwaysRerun; trace (takeFileName out); writeFile' out $ takeFileName out
+    "ids/source" %> \out -> return ()
+    "ids/out" %> \out -> do need =<< readFileLines "ids/source"; writeFile' out ""
+    "ids/*" %> \out -> do alwaysRerun; trace (takeFileName out); writeFile' out $ takeFileName out
 
-    phony (obj "foo") $
-        liftIO $ createDirectoryIfMissing True $ obj "foo"
+    phony "foo" $
+        liftIO $ createDirectoryIfMissing True "foo"
 
     phony "ordering2" $
-        liftIO $ appendFile (obj "order.log") "X"
+        liftIO $ appendFile "order.log" "X"
     phony "ordering" $ do
-        liftIO $ appendFile (obj "order.log") "Y"
+        liftIO $ appendFile "order.log" "Y"
         need ["ordering2"]
 
 test build = do
-    let obj = id
-    writeFile (obj "A.txt") "AAA"
-    writeFile (obj "B.txt") "BBB"
+    writeFile "A.txt" "AAA"
+    writeFile "B.txt" "BBB"
     build ["AB.txt","--sleep"]
-    assertContents (obj "AB.txt") "AAABBB"
-    appendFile (obj "A.txt") "aaa"
+    assertContents "AB.txt" "AAABBB"
+    appendFile "A.txt" "aaa"
     build ["AB.txt"]
-    assertContents (obj "AB.txt") "AAAaaaBBB"
-    removeFile $ obj "AB.txt"
+    assertContents "AB.txt" "AAAaaaBBB"
+    removeFile "AB.txt"
     build ["AB.txt"]
-    assertContents (obj "AB.txt") "AAAaaaBBB"
+    assertContents "AB.txt" "AAAaaaBBB"
 
-    writeFile (obj "zero.txt") "xxx"
+    writeFile "zero.txt" "xxx"
     build ["twice.txt","--sleep"]
-    assertContents (obj "twice.txt") "xxx"
-    writeFile (obj "zero.txt") "yyy"
+    assertContents "twice.txt" "xxx"
+    writeFile "zero.txt" "yyy"
     build ["once.txt","--sleep"]
-    assertContents (obj "twice.txt") "xxx"
-    assertContents (obj "once.txt") "yyy"
-    writeFile (obj "zero.txt") "zzz"
+    assertContents "twice.txt" "xxx"
+    assertContents "once.txt" "yyy"
+    writeFile "zero.txt" "zzz"
     build ["once.txt","twice.txt","--sleep"]
-    assertContents (obj "twice.txt") "zzz"
-    assertContents (obj "once.txt") "zzz"
+    assertContents "twice.txt" "zzz"
+    assertContents "once.txt" "zzz"
 
-    removeFile $ obj "twice.txt"
+    removeFile "twice.txt"
     build ["twice.txt"]
-    assertContents (obj "twice.txt") "zzz"
+    assertContents "twice.txt" "zzz"
 
     show shakeOptions === show shakeOptions
 
@@ -130,67 +127,67 @@ test build = do
     sleep 1 -- sometimes takes a while for the file system to notice
     assertBoolIO (not <$> IO.doesDirectoryExist "dir") "Directory should not exist, cleaner should have removed it"
 
-    writeFile (obj "zero.txt") ""
-    writeFile (obj "configure") ""
-    writeFile (obj "install") ""
+    writeFile "zero.txt" ""
+    writeFile "configure" ""
+    writeFile "install" ""
     build ["configure"]
     build ["install"]
     build ["install"]
-    assertContents (obj "configure") "111"
-    assertContents (obj "install") "11"
+    assertContents "configure" "111"
+    assertContents "install" "11"
 
-    writeFile (obj "dummy.txt") ""
+    writeFile "dummy.txt" ""
     build ["dummy"]
-    assertContents (obj "dummy") "1"
+    assertContents "dummy" "1"
     build ["dummy"]
-    assertContents (obj "dummy") "11"
+    assertContents "dummy" "11"
     build ["dummy","dummy"]
-    assertContents (obj "dummy") "111"
+    assertContents "dummy" "111"
 
-    writeFile (obj "dummer.txt") ""
+    writeFile "dummer.txt" ""
     build ["dummer.txt"]
-    assertContents (obj "dummer.txt") "1"
+    assertContents "dummer.txt" "1"
     build ["dummer.txt"]
-    assertContents (obj "dummer.txt") "11"
+    assertContents "dummer.txt" "11"
 
     build ["1.par","2.par","-j1"]
-    assertContents (obj ".log") "[][]"
-    writeFile (obj ".log") ""
+    assertContents ".log" "[][]"
+    writeFile ".log" ""
     build ["3.par","4.par","-j2"]
-    assertContents (obj ".log") "[[]]"
-    writeFile (obj ".log") ""
+    assertContents ".log" "[[]]"
+    writeFile ".log" ""
     processors <- getProcessorCount
     putStrLn $ "getProcessorCount returned " ++ show processors
     when (processors > 1) $ do
         build ["5.par","6.par","-j0"]
-        assertContents (obj ".log") "[[]]"
+        assertContents ".log" "[[]]"
 
-    writeFile (obj ".log") ""
+    writeFile ".log" ""
     build ["unsafe1.par","unsafe2.par","-j2"]
-    assertContents (obj ".log") "[[]]"
+    assertContents ".log" "[[]]"
 
     build ["threads","-j3"]
-    assertContents (obj "threads.txt") "3"
+    assertContents "threads.txt" "3"
     build ["threads","-j0"]
-    assertContents (obj "threads.txt") (show processors)
+    assertContents "threads.txt" (show processors)
 
-    writeFile (obj "duplicate") ""
+    writeFile "duplicate" ""
     build ["duplicate1","duplicate3"]
-    assertContents (obj "duplicate") "1"
+    assertContents "duplicate" "1"
 
     build $ concat [["sep/" ++ show i ++ ".txt", "sep" </> show i ++ ".txt"] | i <- [1..7]]
 
     build ["slash" </> "platform","slash" </> "forward"]
     build ["slash/platform","slash/forward"]
 
-    createDirectoryIfMissing True (obj "ids")
-    writeFile (obj "ids/source") (obj "ids/a")
+    createDirectoryIfMissing True "ids"
+    writeFile "ids/source" "ids/a"
     build ["ids/out","--sleep"]
-    writeFile (obj ".log") ""
-    writeFile (obj "ids/source") (obj "ids/b")
+    writeFile ".log" ""
+    writeFile "ids/source" "ids/b"
     build ["ids/out","-j4"]
     -- if you collapse depends to [Id] then this ends up asking for the stale 'a'
-    assertContents (obj ".log") "b"
+    assertContents ".log" "b"
 
     build ["foo"]
     build ["foo"]
@@ -198,8 +195,8 @@ test build = do
     build [] -- should say "no want/action statements, nothing to do" (checked manually)
 
     -- #523, #524 - phony children should not run first
-    writeFile (obj "order.log") ""
+    writeFile "order.log" ""
     build ["ordering"]
-    assertContents (obj "order.log") "YX"
+    assertContents "order.log" "YX"
     build ["ordering"]
-    assertContents (obj "order.log") "YXYX"
+    assertContents "order.log" "YXYX"
