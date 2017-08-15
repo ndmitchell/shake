f8985be8c8021c656e6848cd6ab04100237ba741
Neil Mitchell <ndmitchell@gmail.com>
2017-08-15 22:00:19 +0100

    Add ConstraintKinds

--- src/Development/Shake/Internal/Rules/Directory.hs
+++ src/Development/Shake/Internal/Rules/Directory.hs
@@ -1,5 +1,5 @@
 {-# LANGUAGE GeneralizedNewtypeDeriving, ScopedTypeVariables, DeriveDataTypeable #-}
-{-# LANGUAGE TypeFamilies #-}
+{-# LANGUAGE TypeFamilies, ConstraintKinds #-}
 
 -- | Both System.Directory and System.Environment wrappers
 module Development.Shake.Internal.Rules.Directory(
