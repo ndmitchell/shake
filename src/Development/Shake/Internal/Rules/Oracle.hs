4589fb689f889de399df8ae0fd87befe95d7492f
Neil Mitchell <ndmitchell@gmail.com>
2017-08-15 22:00:59 +0100

    Put back in more constraint kinds

--- src/Development/Shake/Internal/Rules/Oracle.hs
+++ src/Development/Shake/Internal/Rules/Oracle.hs
@@ -1,5 +1,5 @@
 {-# LANGUAGE GeneralizedNewtypeDeriving, DeriveDataTypeable, ScopedTypeVariables #-}
-{-# LANGUAGE TypeFamilies #-}
+{-# LANGUAGE TypeFamilies, ConstraintKinds #-}
 
 module Development.Shake.Internal.Rules.Oracle(
     addOracle, askOracle, askOracleWith
