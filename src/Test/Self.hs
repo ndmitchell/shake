7ec12a1fb00c95da9960154a81013119b07d1070
Neil Mitchell <ndmitchell@gmail.com>
2017-11-22 06:24:54 +0000

    #549, make semigroup available in the self test

--- src/Test/Self.hs
+++ src/Test/Self.hs
@@ -90,4 +90,5 @@ cabalBuildDepends _ = packages ++ ["unix" | os /= "mingw32"]
 packages = words
     ("base transformers binary unordered-containers hashable time bytestring primitive " ++
      "filepath directory process deepseq random utf8-string extra js-jquery js-flot") ++
-    ["old-time" | compilerVersion < makeVersion [7,6]]
+    ["old-time" | compilerVersion < makeVersion [7,6]] ++
+    ["semigroup" | compilerVersion < makeVersion [8,0]]
