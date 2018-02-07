f5cab97940abe41fca4fcf32dd7cfd55779ea59e
Neil Mitchell <ndmitchell@gmail.com>
2018-02-07 20:32:07 +0000

    Fix the doc printer

--- src/Test/Docs.hs
+++ src/Test/Docs.hs
@@ -126,7 +126,7 @@ main = shakeTest_ (unless brokenHaddock . noTest) $ do
             ,"var = undefined"
             ,"str = \"\""] ++
             rest
-        liftIO . putStrLn =<< readFile' out
+        liftIO $ putStrLn =<< readFile out
 
     "Files.lst" %> \out -> do
         need [root </> "src/Test/Docs.hs"] -- so much of the generator is in this module
