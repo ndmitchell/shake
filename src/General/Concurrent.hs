67acd9c1611b76c4a28046e4aa3df06957ffb38e
Neil Mitchell <ndmitchell@gmail.com>
2018-04-22 22:42:06 +0100

    Optimise Fence a bit

--- src/General/Concurrent.hs
+++ src/General/Concurrent.hs
@@ -13,20 +13,20 @@ import Prelude
 -- FENCE
 
 -- | Like a barrier, but based on callbacks
-newtype Fence a = Fence (IORef (Either [a -> IO ()] a))
+newtype Fence a = Fence (IORef (Either (a -> IO ()) a))
 instance Show (Fence a) where show _ = "Fence"
 
 newFence :: IO (Fence a)
-newFence = Fence <$> newIORef (Left [])
+newFence = Fence <$> newIORef (Left $ const $ return ())
 
 signalFence :: Fence a -> a -> IO ()
 signalFence (Fence ref) v = join $ atomicModifyIORef ref $ \x -> case x of
-    Left queue -> (Right v, mapM_ ($ v) $ reverse queue)
+    Left queue -> (Right v, queue v)
     Right v -> error "Shake internal error, signalFence called twice on one Fence"
 
 waitFence :: Fence a -> (a -> IO ()) -> IO ()
 waitFence (Fence ref) call = join $ atomicModifyIORef ref $ \x -> case x of
-    Left queue -> (Left (call:queue), return ())
+    Left queue -> (Left (\a -> queue a >> call a), return ())
     Right v -> (Right v, call v)
 
 testFence :: Fence a -> IO (Maybe a)
