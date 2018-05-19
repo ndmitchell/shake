30ad6635fc1c1e705866c43df4d1173284664a51
Neil Mitchell <ndmitchell@gmail.com>
2018-05-19 07:49:34 +0100

    Add BS_Key alias

--- src/Development/Shake/Internal/History/Types.hs
+++ src/Development/Shake/Internal/History/Types.hs
@@ -1,9 +1,10 @@
 
 module Development.Shake.Internal.History.Types(
-    BS_Store, BS_Identity
+    BS_Key, BS_Store, BS_Identity
     ) where
 
 import qualified Data.ByteString as BS
 
+type BS_Key = BS.ByteString
 type BS_Store = BS.ByteString
 type BS_Identity = BS.ByteString
