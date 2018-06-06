75f62cbf285615b4df08c6f3dc5ee7d3bfde961a
Neil Mitchell <ndmitchell@gmail.com>
2018-06-06 17:22:21 +0100

    Switch to using ghc -M in selfmake

--- src/Test/SelfMake.hs
+++ src/Test/SelfMake.hs
@@ -5,11 +5,11 @@ module Test.SelfMake(main) where
 import Development.Shake
 import Development.Shake.Classes
 import Development.Shake.FilePath
+import Development.Shake.Util
 import Test.Type
 
 import Control.Applicative
 import Control.Monad.Extra
-import Data.Char
 import Data.List.Extra
 import System.Info
 import Data.Version.Extra
@@ -23,11 +23,7 @@ type instance RuleResult GhcPkg = [String]
 type instance RuleResult GhcFlags = [String]
 
 main = shakeTest_ noTest $ do
-    let moduleToFile ext xs = replace "." "/" xs <.> ext
     want ["Main" <.> exe]
-
-    -- fixup to cope with Cabal's generated files
-    let fixPaths x = if x == "Paths_shake.hs" then "Paths.hs" else x
 
     ghcPkg <- addOracleHash $ \GhcPkg{} -> do
         Stdout out <- quietly $ cmd "ghc-pkg list --simple-output"
@@ -43,27 +39,16 @@ main = shakeTest_ noTest $ do
             cmd "ghc" flags args
 
     "Main" <.> exe %> \out -> do
-        src <- readFileLines "Run.deps"
-        let hs = [root </> "src" </> fixPaths (moduleToFile "hs" x) | x <- "Run" : src]
+        let run = root </> "src/Run.hs"
         copyFileChanged (root </> "src" </> "Paths.hs") "Paths_shake.hs"
-        need hs
-        ghc ["-o",out,head hs,"-j4"
-            ,"-i" ++ root </> "src","-main-is","Run.main"
-            ,"-hide-all-packages","-outputdir=."
-            ,"-DPORTABLE","-fwarn-unused-imports","-Werror"] -- to test one CPP branch
+        let flags =
+                ["-i" ++ root </> "src","-dep-suffix=.","-main-is","Run.main"
+                ,"-hide-all-packages","-outputdir=."
+                ,"-DPORTABLE","-fwarn-unused-imports","-Werror"] -- to test one CPP branch
 
-    "//*.deps" %> \out -> do
-        dep <- readFileLines $ out -<.> "dep"
-        let xs = map (moduleToFile "deps") dep
-        need xs
-        ds <- nubOrd . sort . (++) dep <$> concatMapM readFileLines xs
-        writeFileLines out ds
-
-    "//*.dep" %> \out -> do
-        src <- readFile' $ root </> "src" </> fixPaths (out -<.> "hs")
-        let xs = hsImports src
-        xs <- filterM (doesFileExist . (\x -> root </> "src" </> x) . fixPaths . moduleToFile "hs") xs
-        writeFileLines out xs
+        ghc $ ["-M",run] ++ flags
+        need . filter (\x -> takeExtension x == ".hs") . concatMap snd . parseMakefile =<< liftIO (readFile "Makefile")
+        ghc $ ["-o",out,run,"-j4"] ++ flags
 
     ".pkgs" %> \out -> do
         src <- readFile' $ root </> "shake.cabal"
@@ -72,11 +57,6 @@ main = shakeTest_ noTest $ do
 
 ---------------------------------------------------------------------
 -- GRAB INFORMATION FROM FILES
-
-hsImports :: String -> [String]
-hsImports xs = [ takeWhile (\x -> isAlphaNum x || x `elem` "._") $ dropWhile (not . isUpper) x
-               | x <- concatMap (wordsBy (== ';')) $ lines xs, "import " `isPrefixOf` trim x]
-
 
 -- FIXME: Should actually parse the list from the contents of the .cabal file
 cabalBuildDepends :: String -> [String]
