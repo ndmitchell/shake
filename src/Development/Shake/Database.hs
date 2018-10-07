e49aeba035d51bf4234eba946f98b0ccef4a8376
Neil Mitchell <ndmitchell@gmail.com>
2018-10-07 17:52:47 +0100

    Make sure Database gives good error messages in all the possible error cases

--- src/Development/Shake/Database.hs
+++ src/Development/Shake/Database.hs
@@ -1,5 +1,6 @@
+{-# LANGUAGE RecordWildCards #-}
 
--- | Module in development
+-- |
 module Development.Shake.Database(
     ShakeDatabase,
     shakeOpenDatabase,
@@ -10,6 +11,7 @@ module Development.Shake.Database(
     shakeRunAfter
     ) where
 
+import Control.Concurrent.Extra
 import Control.Exception
 import Control.Monad
 import Control.Monad.IO.Class
@@ -24,16 +26,41 @@ import Development.Shake.Internal.Core.Types
 import Development.Shake.Internal.Rules.Default
 import Prelude
 
+data UseState
+    = Closed
+    | Using String
+    | Open {openOneShot :: Bool, openRequiresReset :: Bool}
 
-newtype ShakeDatabase = ShakeDatabase RunState
+
+data ShakeDatabase = ShakeDatabase (Var UseState) RunState
 
 shakeOpenDatabase :: ShakeOptions -> Rules () -> IO (IO ShakeDatabase, IO ())
 shakeOpenDatabase opts rules = do
     (cleanup, clean) <- newCleanup
-    return (ShakeDatabase <$> open cleanup opts (rules >> defaultRules), clean)
+    use <- newVar $ Open False False
+    let alloc =
+            ShakeDatabase use <$> open cleanup opts (rules >> defaultRules)
+    let free = do
+            modifyVar_ use $ \x -> case x of
+                    Using s -> fail $ "Can't close while running, " ++ s
+                    _ -> return Closed
+            clean
+    return (alloc, free)
+
+withOpen :: Var UseState -> String -> (UseState -> UseState) -> (UseState -> IO a) -> IO a
+withOpen var name final act = mask $ \restore -> do
+    o <- modifyVar var $ \x -> case x of
+        Using s -> fail "Already using"
+        Closed -> fail "Already closed"
+        o@Open{} -> return (Using name, o)
+    let clean = writeVar var $ final o
+    res <- restore (act o) `onException` clean
+    clean
+    return res
 
 shakeOneShotDatabase :: ShakeDatabase -> IO ()
-shakeOneShotDatabase _ = return ()
+shakeOneShotDatabase (ShakeDatabase use db) =
+    withOpen use "shakeOneShotDatabase" (\o -> o{openOneShot=True}) $ \_ -> return ()
 
 shakeWithDatabase :: ShakeOptions -> Rules () -> (ShakeDatabase -> IO a) -> IO a
 shakeWithDatabase opts rules act = do
@@ -41,16 +68,22 @@ shakeWithDatabase opts rules act = do
     (act =<< db) `finally` clean
 
 shakeLiveFilesDatabase :: ShakeDatabase -> IO [FilePath]
-shakeLiveFilesDatabase (ShakeDatabase s) = liveFilesState s
+shakeLiveFilesDatabase (ShakeDatabase use s) =
+    withOpen use "shakeLiveFilesDatabase" id $ \_ ->
+        liveFilesState s
 
 shakeRunDatabase :: ShakeDatabase -> [Action a] -> IO ([a], [IO ()])
-shakeRunDatabase (ShakeDatabase s) as = do
-    (refs, as) <- fmap unzip $ forM as $ \a -> do
-        ref <- newIORef Nothing
-        return (ref, liftIO . writeIORef ref . Just =<< a)
-    let oneShot = True
-    after <- run s oneShot $ map void as
-    results <- mapM readIORef refs
-    case sequence results of
-        Just result -> return (result, after)
-        Nothing -> throwM $ errorInternal "Expected all results were written, but some where not"
+shakeRunDatabase (ShakeDatabase use s) as =
+    withOpen use "shakeRunDatabase" (\o -> o{openRequiresReset=True}) $ \Open{..} -> do
+        when openRequiresReset $ do
+            when openOneShot $
+                fail "You applied one shot and you're running it twice"
+            reset s
+        (refs, as) <- fmap unzip $ forM as $ \a -> do
+            ref <- newIORef Nothing
+            return (ref, liftIO . writeIORef ref . Just =<< a)
+        after <- run s openOneShot $ map void as
+        results <- mapM readIORef refs
+        case sequence results of
+            Just result -> return (result, after)
+            Nothing -> throwM $ errorInternal "Expected all results were written, but some where not"
