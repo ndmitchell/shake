3115ac110d8c3ba2c85e17e9face95204f13fa01
Neil Mitchell <ndmitchell@gmail.com>
2018-10-11 20:48:05 +0100

    Switch from MVar to Var in one of the tests

--- src/Test/Pool.hs
+++ src/Test/Pool.hs
@@ -21,15 +21,15 @@ test build = do
     forM_ [False,True] $ \deterministic -> do
         -- check that it aims for exactly the limit
         forM_ [1..6] $ \n -> do
-            var <- newMVar (0,0) -- (maximum, current)
+            var <- newVar (0,0) -- (maximum, current)
             runPool deterministic n $ \pool ->
                 forM_ [1..5] $ \i ->
                     add pool $ do
-                        modifyMVar_ var $ \(mx,now) -> return (max (now+1) mx, now+1)
+                        modifyVar_ var $ \(mx,now) -> return (max (now+1) mx, now+1)
                         -- requires that all tasks get spawned within 0.1s
                         sleep 0.1
-                        modifyMVar_ var $ \(mx,now) -> return (mx,now-1)
-            res <- takeMVar var
+                        modifyVar_ var $ \(mx,now) -> return (mx,now-1)
+            res <- readVar var
             res === (min n 5, 0)
 
         -- check that exceptions are immediate
