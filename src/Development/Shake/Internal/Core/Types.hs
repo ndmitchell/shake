a6cb26cab920311fa905973077004ea26545b669
Neil Mitchell <ndmitchell@gmail.com>
2018-11-28 22:02:00 +0000

    UNPACK the OneShot variable in Global

--- src/Development/Shake/Internal/Core/Types.hs
+++ src/Development/Shake/Internal/Core/Types.hs
@@ -402,7 +402,7 @@ data Global = Global
     ,globalShared :: Maybe Shared -- ^ The active shared state, if any
     ,globalCloud :: Maybe Cloud
     ,globalStep :: {-# UNPACK #-} !Step
-    ,globalOneShot :: Bool -- ^ I am running in one-shot mode so don't need to store BS's for Result/Error
+    ,globalOneShot :: {-# UNPACK #-} !Bool -- ^ I am running in one-shot mode so don't need to store BS's for Result/Error
     }
 
 -- local variables of Action
