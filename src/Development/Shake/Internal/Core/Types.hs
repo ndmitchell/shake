a3715c01b3840393962a45b9f0cfe63f4865230e
Neil Mitchell <ndmitchell@gmail.com>
2018-11-28 22:38:13 +0000

    Remove an unusable UNPACK

--- src/Development/Shake/Internal/Core/Types.hs
+++ src/Development/Shake/Internal/Core/Types.hs
@@ -402,7 +402,7 @@ data Global = Global
     ,globalShared :: Maybe Shared -- ^ The active shared state, if any
     ,globalCloud :: Maybe Cloud
     ,globalStep :: {-# UNPACK #-} !Step
-    ,globalOneShot :: {-# UNPACK #-} !Bool -- ^ I am running in one-shot mode so don't need to store BS's for Result/Error
+    ,globalOneShot :: Bool -- ^ I am running in one-shot mode so don't need to store BS's for Result/Error
     }
 
 -- local variables of Action
