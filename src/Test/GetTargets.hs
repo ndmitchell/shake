ada33849db60f5bdc9fcd689d921f170cffd0d28
Neil Mitchell <ndmitchell@gmail.com>
2019-02-16 15:41:43 +0000

    Indentation whitespace

--- src/Test/GetTargets.hs
+++ src/Test/GetTargets.hs
@@ -10,22 +10,22 @@ main _sleeper = do
 
 rules :: Rules ()
 rules = do
-  withTargetDocs "A phony target" $ phony "phony1" $ return ()
+    withTargetDocs "A phony target" $ phony "phony1" $ return ()
 
-  "file1" %> \_ -> return ()
-  ["file2", "file3"] |%> \_ -> return ()
-  ["file4", "file5"] &%> \_ -> return ()
+    "file1" %> \_ -> return ()
+    ["file2", "file3"] |%> \_ -> return ()
+    ["file4", "file5"] &%> \_ -> return ()
 
-  "file6" %> \_ -> return ()
-  ["file7", "file8"] |%> \_ -> return ()
-  ["file9", "file10"] &%> \_ -> return ()
+    "file6" %> \_ -> return ()
+    ["file7", "file8"] |%> \_ -> return ()
+    ["file9", "file10"] &%> \_ -> return ()
 
-  withTargetDocs "Builds something really good" $ phony "phony2" $ return ()
-  withTargetDocs "bad docs" $ do
-    withTargetDocs "a great file" $ "file11" %> \_ -> return ()
-    withTargetDocs "awesome files" $ ["file12", "file13"] &%> \_ -> return ()
-    phony "Foo" $ return ()
-    withoutTargets $ phony "Bar" $ return ()
+    withTargetDocs "Builds something really good" $ phony "phony2" $ return ()
+    withTargetDocs "bad docs" $ do
+        withTargetDocs "a great file" $ "file11" %> \_ -> return ()
+        withTargetDocs "awesome files" $ ["file12", "file13"] &%> \_ -> return ()
+        phony "Foo" $ return ()
+        withoutTargets $ phony "Bar" $ return ()
 
 
 expected :: [(String, Maybe String)]
