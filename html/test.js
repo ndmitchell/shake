acb827c0ba9556528ca1773ac11a696b78d6e7dd
Neil Mitchell <ndmitchell@gmail.com>
2019-02-18 21:28:05 +0000

    Add a test that can run headless

---
+++ html/test.js
@@ -0,0 +1,19 @@
+// RUN TEST USING:
+// pushd ts && tsc && popd && node test.js
+
+var fs = require("fs");
+var vm = require("vm");
+
+function include(path) {
+    var code = fs.readFileSync(path, 'utf-8');
+    vm.runInThisContext(code, global);
+}
+
+// Stub enough things to ignore UI pieces
+
+global.window = {};
+global.jQuery = {fn : {}};
+
+include("profile-data.js");
+include("shake.js", "test();");
+test();
