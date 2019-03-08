a77c3a31d97fb70028c7bce6cf1fcf6abf895d6b
Neil Mitchell <ndmitchell@gmail.com>
2019-03-08 09:40:13 +0000

    Simplify the command, split out data from UI

--- html/ts/reports/cmdtable.tsx
+++ html/ts/reports/cmdtable.tsx
@@ -1,9 +1,16 @@
 
 function reportCmdTable(profile: Profile[], search: Prop<Search>): HTMLElement {
-    return cmdTableData(search.get());
+    const columns: Column[] =
+        [ {field: "name", label: "Name", width: 200}
+        , {field: "count", label: "Count", width: 75, alignRight: true, show: showInt}
+        , {field: "total", label: "Total", width: 75, alignRight: true, show: showTime}
+        , {field: "average", label: "Average", width: 75, alignRight: true, show: showTime}
+        , {field: "max", label: "Max", width: 75, alignRight: true, show: showTime}
+        ];
+    return newTable(columns, new Prop(cmdTableData(search.get())));
 }
 
-function cmdTableData(search: Search): HTMLElement {
+function cmdTableData(search: Search): object[] {
     const res: MapString< {count: int, total: seconds, max: seconds} > = {};
     search.forEachProfile(p =>
         p.traces.forEach(t => {
@@ -18,15 +25,8 @@ function cmdTableData(search: Search): HTMLElement {
             }
         })
     );
-    const columns: Column[] =
-        [ {field: "name", label: "Name", width: 200}
-        , {field: "count", label: "Count", width: 75, alignRight: true, show: showInt}
-        , {field: "total", label: "Total", width: 75, alignRight: true, show: showTime}
-        , {field: "average", label: "Average", width: 75, alignRight: true, show: showTime}
-        , {field: "max", label: "Max", width: 75, alignRight: true, show: showTime}
-        ];
     const res2 = [];
     for (const i in res)
         res2.push({name: i, average: res[i].total / res[i].count, ...res[i]});
-    return newTable(columns, new Prop(res2));
+    return res2;
 }
