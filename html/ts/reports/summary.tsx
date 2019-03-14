7e5b87f47a5cdbbd3c1c637c3b23e2d8a0c080b0
Neil Mitchell <ndmitchell@gmail.com>
2019-03-14 20:32:39 +0000

    Add a precise critical path to the profiling

--- html/ts/reports/summary.tsx
+++ html/ts/reports/summary.tsx
@@ -34,7 +34,8 @@ function reportSummary(profile: Profile[]): HTMLElement {
             <li><b>Build time:</b> {showTime(sumExecution)} <span class="note">how long a complete build would take single threaded.</span></li>
             <li><b>Last build time:</b> {showTime(maxTraceStopLast)} <span class="note">how long the last build take.</span></li>
             <li><b>Parallelism:</b> {(maxTraceStopLast === 0 ? 0 : sumExecutionLast / maxTraceStopLast).toFixed(2)} <span class="note">average number of commands executing simultaneously in the last build.</span></li>
-            <li><b>Critical path:</b> {showTime(speculativeCriticalPath(profile))} <span class="note">how long it would take on infinite CPUs.</span></li>
+            <li><b>Speculative critical path:</b> {showTime(speculativeCriticalPath(profile))} <span class="note">how long it would take on infinite CPUs.</span></li>
+            <li><b>Precise critical path:</b> {showTime(preciseCriticalPath(profile))} <span class="note">critical path not speculatively executing.</span></li>
         </ul>
     </div>;
 }
@@ -52,4 +53,83 @@ function speculativeCriticalPath(profile: Profile[]): seconds {
         criticalPath[p.index] = cost;
     }
     return maxCriticalPath;
+}
+
+/*
+Calculating a precise critical path, taking into account the deep dependeny structure, is non-obvious.
+Dependencies have the type [{X}], e.g:
+
+    X = [{a,b},{c,d}]
+
+That is r builds a and b, then after those both complete (assuming they don't change), it builds c and d,
+then it is finished. Importantly, r doesn't start building c/d until after a and b have finished. This
+detail extends the critical path.
+
+To calculate the precise critical path, we simulate with the notion of demand and waiting.
+*/
+function preciseCriticalPath(profile: Profile[]): seconds {
+    const waiting: int[] = profile.map(x => x.depends.concatLength()) ; // number I am waiting on before I am done
+    const demanded: boolean[] = []; // I have been demanded by someone
+    const oncomplete: Array<() => void> = []; // Completion functions
+    const complete: boolean[] = []; // Who is complete already
+    const running: Array<[pindex, seconds]> = [];
+    let timestamp: seconds = 0;
+
+    // demand dependency set N of a rule
+    function demandN(p: Profile, round: int): void {
+        for (; round < p.depends.length; round++) {
+            let todo = p.depends[round].length; // Number before we continue
+            const step = () => {
+                todo--;
+                if (todo === 0)
+                    demandN(p, round + 1);
+            };
+            for (const d of p.depends[round]) {
+                if (complete[d])
+                    todo--;
+                else {
+                    const old = oncomplete[d];
+                    oncomplete[d] = !old ? step : () => { old(); step(); };
+                    demand(profile[d]);
+                }
+            }
+            if (todo !== 0) break;
+            // todo === 0, so continue (equivalent to calling step but tail recursive)
+        }
+    }
+
+    // demand a particular rule
+    function demand(p: Profile): void {
+        if (demanded[p.index]) return;
+        demanded[p.index] = true;
+        if (waiting[p.index] === 0)
+            running.insertSorted([p.index, timestamp + p.execution], compareSndRev);
+        else
+            demandN(p, 0);
+    }
+
+    // We don't know the targets we ask for, so we approximate by saying the ones which nothing depends on
+    for (const p of profile) {
+        if (p.rdepends.length === 0)
+            demand(p);
+    }
+
+    while (running.length > 0) {
+        const [ind, time] = running.pop();
+        timestamp = time;
+        complete[ind] = true;
+        if (oncomplete[ind]) {
+            oncomplete[ind]();
+            delete oncomplete[ind];
+        }
+        for (const d of profile[ind].rdepends) {
+            waiting[d]--;
+            if (waiting[d] === 0 && demanded[d])
+                running.insertSorted([d, timestamp + profile[d].execution], compareSndRev);
+        }
+    }
+    for (let i = 0; i < profile.length; i++)
+        if (!complete[i])
+            throw new Error("Failed to run all tasks");
+    return timestamp;
 }
