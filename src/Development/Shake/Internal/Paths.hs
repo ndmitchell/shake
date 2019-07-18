8bee00debfb98d827c87241b0b45557e24d01d1a
Austin Seipp <as@fastly.com>
2019-07-17 20:30:07 -0500

    shake: embed data files into 'shake' executable
    This patch directly embeds the set of needed re-distributable artifacts
    in the Shake repository into the 'shake' executable by default, and,
    optionally, the 'shake' library as well.
    
    Without this change, Shake binaries cannot build HTML reports without
    the Haskell toolchain installed. This means that users of Shake for e.g.
    ninja compatibility are unable to build reports transparently.
    
    There have been ideas floated around to embed data files at the end of
    the binary, but this seems needlessly complicated. Instead, this patch
    simply embeds the files in the binary at compile time if asked, using
    Template Haskell.
    
    One complicating factor that made this change hard in the past is that
    GHC is moving to use Hadrian (and Shake) by default, and so adding a
    stage2 feature dependency (TemplateHaskell requires GHCi) seriously
    hurts the ability to bootstrap GHC: you would need an entire stage2
    cross toolchain to build Hadrian, for instance.
    
    However, there are some crucial distinctions to make here, and some
    factors that make this change not so bad:
    
      - The `shake` executable, which provides Ninja compatibility, is NOT
      bound by the Hadrian/Haskell stage2 requirements. This means that even
      e.g.  CMake or external users of Shake can still benefit from such a
      change, if it is done carefully.
    
      - The `shake` library, which is what developers use to write build
      systems, MAY be bound by the stage2 restriction, i.e. cannot use
      Template Haskell, due to platform restrictions. But it also may not
      be; a user on x86_64-linux who is writing a custom build system may
      well want to deliver it to their developers, also running
      x86_64-linux.
    
      - Users of Haskell tooling, such as Stack or Cabal (new-build),
      generally have the ability to set per-package compilation flags for
      their projects. For instance, a single project using `cabal new-build`
      can globally enforce the usage of Shake, but only compiled with a
      specific set of build-time flags. While this wasn't always available,
      it is now generally widely available.
    
    The third mitigating factor is what motivates this solution as the
    simplest and easiest: to simply add support and help users in group 1
    (currently myself), and also some of the users in group 2 by simply
    requiring them to set flags for the dependent package.
    
    In this setup, the `shake` library, by default, still DOES NOT use file
    embedding. Rather, setting the flag `-fembed-files` at build time will
    enable support for it in the library. Users of Shake who want to create
    binaries that have profiling support "included" can simply enforce their
    projects to have this flag enabled, and their binaries will always
    support this feature.
    
    The `shake` executable, however, ALWAYS has support for file-embedding
    built in. This executable is generally useful only for Ninja
    compatibility (or general demo purposes), so adding this requirement
    does not seem especially onerous, and improves the lives of any user who
    is currently depends on it.
    
    This patch has been tested on a full CMake/Ninja build on a standalone
    Ubuntu 16.04 machine, and was able to generate a working HTML build
    report.
    
    Closes #268. Closes #413.
    
    Signed-off-by: Austin Seipp <as@fastly.com>

--- src/Development/Shake/Internal/Paths.hs
+++ src/Development/Shake/Internal/Paths.hs
@@ -1,3 +1,8 @@
+{-# LANGUAGE CPP #-}
+
+#ifdef FILE_EMBED
+{-# LANGUAGE TemplateHaskell #-}
+#endif
 
 -- | The information from Paths_shake cleaned up
 module Development.Shake.Internal.Paths(
@@ -7,7 +12,6 @@ module Development.Shake.Internal.Paths(
     readDataFileHTML
     ) where
 
-import Paths_shake
 import Control.Exception
 import Control.Monad.Extra
 import Data.Version
@@ -18,12 +22,47 @@ import System.IO.Unsafe
 import System.Environment
 import General.Extra
 import qualified Data.ByteString.Lazy as LBS
+import Paths_shake
 
+#ifdef FILE_EMBED
+import qualified Data.ByteString as BS
+import Data.FileEmbed
+#endif
 
 shakeVersionString :: String
 shakeVersionString = showVersion version
 
+#ifdef FILE_EMBED
 
+initDataDirectory :: IO ()
+initDataDirectory = return ()
+
+htmlDataFiles :: [(FilePath, BS.ByteString)]
+htmlDataFiles =
+  [ ("profile.html",  $(embedFile "html/profile.html"))
+  , ("progress.html", $(embedFile "html/progress.html"))
+  , ("shake.js",      $(embedFile "html/shake.js"))
+  ]
+
+readDataFileHTML :: FilePath -> IO LBS.ByteString
+readDataFileHTML file = do
+    case lookup file htmlDataFiles of
+      Nothing -> fail $ "Could not find data file " ++ file ++ " in embedded data files!"
+      Just x  -> return (LBS.fromStrict x)
+
+manualDirData :: [(FilePath, BS.ByteString)]
+manualDirData = $(embedDir "docs/manual")
+
+hasManualData :: IO Bool
+hasManualData = return True
+
+copyManualData :: FilePath -> IO ()
+copyManualData dest = do
+    createDirectoryRecursive dest
+    forM_ manualDirData $ \(file, bs) -> do
+        BS.writeFile (dest </> file) bs
+
+#else
 -- We want getDataFileName to be relative to the current directory on program startup,
 -- even if we issue a change directory command. Therefore, first call caches, future ones read.
 {-# NOINLINE dataDirs #-}
@@ -34,11 +73,9 @@ dataDirs = unsafePerformIO $ do
     curdir <- getCurrentDirectory
     return $ [datdir] ++ [exedir | exedir /= ""] ++ [curdir]
 
-
 -- The data files may be located relative to the current directory, if so cache it in advance
 initDataDirectory :: IO ()
 initDataDirectory = void $ evaluate dataDirs
-
 
 getDataFile :: FilePath -> IO FilePath
 getDataFile file = do
@@ -51,13 +88,11 @@ getDataFile file = do
 hasDataFile :: FilePath -> IO Bool
 hasDataFile file = anyM (\dir -> doesFileExist_ $ dir </> file) dataDirs
 
-
 readDataFileHTML :: FilePath -> IO LBS.ByteString
 readDataFileHTML file = LBS.readFile =<< getDataFile ("html" </> file)
 
-
 manualFiles :: [FilePath]
-manualFiles = map ("docs/manual" </>) ["Build.hs","main.c","constants.c","constants.h","build" <.> if isWindows then "bat" else "sh"]
+manualFiles = map ("docs/manual" </>) ["Build.hs","main.c","constants.c","constants.h","build.bat","build.sh"]
 
 hasManualData :: IO Bool
 hasManualData = allM hasDataFile manualFiles
@@ -68,3 +103,4 @@ copyManualData dest = do
     forM_ manualFiles $ \file -> do
         src <- getDataFile file
         copyFile src (dest </> takeFileName file)
+#endif
