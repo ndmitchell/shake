09fc2c03f73730f57e12d2095f693a848432ae1a
Neil Mitchell <ndmitchell@gmail.com>
2020-05-17 12:16:40 +0100

    Delete stuff deprecated since 2014 - **>, ?>>, *>>, *>, |*>, &*>

--- src/Development/Shake.hs
+++ src/Development/Shake.hs
@@ -110,15 +110,11 @@ module Development.Shake(
     batch,
     reschedule,
     -- * Deprecated
-    (*>), (|*>), (&*>),
-    (**>), (*>>), (?>>),
     askOracleWith,
     deprioritize,
     pattern Quiet, pattern Normal, pattern Loud, pattern Chatty,
     putLoud, putNormal, putQuiet
     ) where
-
-import Prelude(Maybe, FilePath, Double, String) -- Since GHC 7.10 duplicates *>
 
 -- I would love to use module export in the above export list, but alas Haddock
 -- then shows all the things that are hidden in the docs, which is terrible.
@@ -218,49 +214,6 @@ import Development.Shake.Internal.Rules.Rerun
 --
 -- * "Development.Shake.Util" has general utilities that are useful for build systems, e.g.
 --   reading @Makefile@ syntax and alternative forms of argument parsing.
-
-
----------------------------------------------------------------------
--- DEPRECATED SINCE 0.13, MAY 2014
-
-infix 1 **>, ?>>, *>>
-
-
-{-# DEPRECATED (**>) "Use |%> instead" #-}
--- | /Deprecated:/ Alias for '|%>'.
-(**>) :: [FilePattern] -> (FilePath -> Action ()) -> Rules ()
-(**>) = (|%>)
-
-{-# DEPRECATED (?>>) "Use &?> instead" #-}
--- | /Deprecated:/ Alias for '&?>'.
-(?>>) :: (FilePath -> Maybe [FilePath]) -> ([FilePath] -> Action ()) -> Rules ()
-(?>>) = (&?>)
-
-{-# DEPRECATED (*>>) "Use &%> instead" #-}
--- | /Deprecated:/ Alias for '&%>'.
-(*>>) :: [FilePattern] -> ([FilePath] -> Action ()) -> Rules ()
-(*>>) = (&%>)
-
-
----------------------------------------------------------------------
--- DEPRECATED SINCE 0.14, MAY 2014
-
-infix 1 *>, |*>, &*>
-
-{-# DEPRECATED (*>) "Use %> instead" #-}
--- | /Deprecated:/ Alias for '%>'. Note that @*>@ clashes with a Prelude operator in GHC 7.10.
-(*>) :: FilePattern -> (FilePath -> Action ()) -> Rules ()
-(*>) = (%>)
-
-{-# DEPRECATED (|*>) "Use |%> instead" #-}
--- | /Deprecated:/ Alias for '|%>'.
-(|*>) :: [FilePattern] -> (FilePath -> Action ()) -> Rules ()
-(|*>) = (|%>)
-
-{-# DEPRECATED (&*>) "Use &%> instead" #-}
--- | /Deprecated:/ Alias for '&%>'.
-(&*>) :: [FilePattern] -> ([FilePath] -> Action ()) -> Rules ()
-(&*>) = (&%>)
 
 
 ---------------------------------------------------------------------
